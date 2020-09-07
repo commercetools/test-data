60edda934348c98242a163865e3b195452a95567
Nicola Molinari <nicola.molinari@commercetools.de>
2020-09-07 18:34:09 +0200

    fix: export

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,5 +1,3 @@
-export * as helpers from './helpers';
-
 export { default as Builder } from './builder';
 export { default as Generator } from './generator';
 export { default as Transformer } from './transformer';
