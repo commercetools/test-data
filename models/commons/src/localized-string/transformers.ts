0af2dfc4628afd674ae4e9809219704c1d56b80a
Fedor Kulikov <f.kulikov@gmail.com>
2020-10-14 23:43:06 +0500

    fix(localized-string): filter out empty translations

--- models/commons/src/localized-string/transformers.ts
+++ models/commons/src/localized-string/transformers.ts
@@ -1,15 +1,19 @@
 import type { TLocalizedString, TLocalizedStringGraphql } from './types';
-
 import { Transformer } from '@commercetools-test-data/core';
+
+const isNil = (value: string | undefined) =>
+  value === undefined || value === null;
 
 const transformers = {
   graphql: Transformer<TLocalizedString, TLocalizedStringGraphql>('graphql', {
     replaceFields: ({ fields }) =>
-      Object.entries(fields).map(([locale, value]) => ({
-        locale,
-        value,
-        __typename: 'LocalizedString',
-      })),
+      Object.entries(fields)
+        .filter(([, value]) => !isNil(value))
+        .map(([locale, value]) => ({
+          locale,
+          value,
+          __typename: 'LocalizedString',
+        })),
   }),
 };
 
