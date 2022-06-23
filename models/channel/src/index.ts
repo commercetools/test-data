abb2acae91ccb274dcc066c73efa7d4e29368b75
Nicola Molinari <nicola.molinari@commercetools.com>
2022-06-23 08:44:03 +0200

    fix: export types (#136)

--- models/channel/src/index.ts
+++ models/channel/src/index.ts
@@ -1,6 +1,7 @@
 import * as ChannelDraft from './channel-draft';
+export { ChannelDraft };
 
 export { default as random } from './builder';
 export * as constants from './constants';
 export { default as presets } from './presets';
-export { ChannelDraft };
+export * from './types';
