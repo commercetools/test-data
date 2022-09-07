d279bdcaf6b4ad5e5c6e6c5f6fa4191ab480a193
Fredrick Mgbeoma <fmgbeoma@yahoo.com>
2022-09-07 16:40:30 +0200

    feat(standalone-prices): add model types

---
+++ models/standalone-price/types.ts
@@ -0,0 +1,22 @@
+import type {
+  StandalonePrice,
+  StandalonePriceDraft,
+} from '@commercetools/platform-sdk';
+import type { TBuilder } from '@commercetools-test-data/core';
+
+export type TStandalonePrice = StandalonePrice;
+export type TStandalonePriceDraft = StandalonePriceDraft;
+
+export type TStandalonePriceGraphQL = TStandalonePrice & {
+  __typename: 'StandalonePrice';
+};
+
+export type TStandalonePriceGraphQLDraft = TStandalonePriceDraft & {
+  __typename: 'StandalonePriceDraft';
+};
+
+export type TStandalonePriceBuilder = TBuilder<TStandalonePrice>;
+export type TStandalonePriceDraftBuilder = TBuilder<TStandalonePriceDraft>;
+export type TCreateStandalonePriceBuilder = () => TStandalonePriceBuilder;
+export type TCreateStandalonePriceDraftBuilder =
+  () => TStandalonePriceDraftBuilder;
