80604902f0c0d8900461905f1f6955acfcb4b2eb
Valorie Carli <valorie.carli@commercetools.com>
2022-12-27 14:41:19 -0600

    chore(category): add category-draft to index

--- models/category/src/index.ts
+++ models/category/src/index.ts
@@ -1,3 +1,6 @@
+import * as CategoryDraft from './category-draft';
+export { CategoryDraft };
+
 export { default as random } from './builder';
 export { default as presets } from './presets';
 export * from './types';
