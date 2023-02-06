25cda14a498ed67ac58f0eb523d37a381660de81
Valorie Carli <valorie.carli@commercetools.com>
2023-02-06 13:32:47 -0600

    chore(package.json): manypkg fix

--- models/tax-category/src/generator.ts
+++ models/tax-category/src/generator.ts
@@ -1,6 +1,6 @@
 import { ClientLogging } from '@commercetools-test-data/commons';
 import { sequence, fake, Generator } from '@commercetools-test-data/core';
-import { random } from '@commercetools-test-data/tax-rate';
+import * as TaxRate from '@commercetools-test-data/tax-rate';
 import { createRelatedDates } from '@commercetools-test-data/utils';
 import { TTaxCategory } from './types';
 
@@ -15,7 +15,7 @@ const generator = Generator<TTaxCategory>({
     key: fake((f) => f.lorem.slug(2)),
     name: fake((f) => f.lorem.words(2)),
     description: fake((f) => f.lorem.sentence()),
-    rates: fake(() => [random()]),
+    rates: fake(() => [TaxRate.random()]),
     createdAt: fake(getOlderDate),
     createdBy: fake(() => ClientLogging.random()),
     lastModifiedAt: fake(getNewerDate),
