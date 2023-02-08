6c9a1d7634cd6aa846c40e5ad73882d77ba4a585
Tyler Ford <tyler.ford@commercetools.com>
2023-02-08 07:38:08 -0700

    fix: adding review export

--- models/review/src/index.ts
+++ models/review/src/index.ts
@@ -1,3 +1,5 @@
+import * as ReviewDraft from './review-draft';
 export { default as random } from './builder';
 export { default as presets } from './presets';
+export { ReviewDraft };
 export * from './types';
