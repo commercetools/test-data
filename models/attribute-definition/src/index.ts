a1cb3ddd35c10c08fc46607f47aa103631e2a40a
Tyler Ford <tyler.ford@commercetools.com>
2023-02-09 15:24:50 -0700

    fix: add export of draft

--- models/attribute-definition/src/index.ts
+++ models/attribute-definition/src/index.ts
@@ -1,3 +1,5 @@
+import * as AttributeDefinitionDraft from './attribute-definition-draft';
 export { default as random } from './builder';
 export { default as presets } from './presets';
+export { AttributeDefinitionDraft };
 export * from './types';
