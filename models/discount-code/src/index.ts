e4a3a07e030308e390682d80ccbc0337a11afae8
jaikamat <jaipkamat@gmail.com>
2023-02-13 10:39:12 -0500

    chore(test-data): expose draft in index file

--- models/discount-code/src/index.ts
+++ models/discount-code/src/index.ts
@@ -1,3 +1,6 @@
+import * as DiscountCodeDraft from './discount-code-draft';
+export { DiscountCodeDraft };
+
 export { default as random } from './builder';
 export { default as presets } from './presets';
 export * from './types';
