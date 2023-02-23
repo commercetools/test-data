d2ca65c2449c10da8dae90795f3459c0bec59edc
jaikamat <jaipkamat@gmail.com>
2023-02-23 14:38:55 -0500

    chore(test-data): remember to export, duh

--- models/attribute/src/index.ts
+++ models/attribute/src/index.ts
@@ -1,3 +1,6 @@
+import * as AttributeDraft from './attribute-draft';
+export { AttributeDraft };
+
 export { default as random } from './builder';
 export { default as presets } from './presets';
 export * from './types';
