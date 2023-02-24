9d52d8b4364158a1c47cd038b3bf41585b6ee58b
Tyler Ford <tyler.ford@commercetools.com>
2023-02-24 10:36:05 -0700

    fix: update TaxRateDraft export, use in preset

--- models/tax-rate/src/index.ts
+++ models/tax-rate/src/index.ts
@@ -1,5 +1,4 @@
-import * as TaxRateDraft from './tax-rate-draft';
-export { TaxRateDraft };
+export * as TaxRateDraft from './tax-rate-draft';
 
 export { default as random } from './builder';
 export { default as presets } from './presets';
