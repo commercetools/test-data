d3fdaeeede09b845eb9c665b979d09886ad2129f
Valorie <47182700+valoriecarli@users.noreply.github.com>
2023-02-28 10:18:26 -0600

    fix(index): allow const access (#207)
    * fix(index): allow const access
    
    * chore(changeset): include changeset

--- models/attribute-definition/src/index.ts
+++ models/attribute-definition/src/index.ts
@@ -2,4 +2,5 @@ import * as AttributeDefinitionDraft from './attribute-definition-draft';
 export { default as random } from './builder';
 export { default as presets } from './presets';
 export { AttributeDefinitionDraft };
+export * as constants from './constants';
 export * from './types';
