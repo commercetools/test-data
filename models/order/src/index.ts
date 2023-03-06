4eff666c334fe49562804be1c0285e9c8032f6bc
jaikamat <jaipkamat@gmail.com>
2023-03-06 11:41:28 -0500

    chore(test-data): export constants

--- models/order/src/index.ts
+++ models/order/src/index.ts
@@ -4,3 +4,4 @@ export { OrderFromCartDraft };
 export { default as random } from './builder';
 export { default as presets } from './presets';
 export * from './types';
+export * as constants from './constants';
