63151c55195439b0a600f42bad82bbc24882f508
Valorie Carli <valorie.carli@commercetools.com>
2023-03-07 15:21:44 -0600

    chore(pkg-preset): remove core and common from aulo preset

--- presets/change-history/src/index.ts
+++ presets/change-history/src/index.ts
@@ -2,8 +2,6 @@ export * as AttributeDefinition from '@commercetools-test-data/attribute-definit
 export * as AttributeType from '@commercetools-test-data/attribute-type';
 export * as Category from '@commercetools-test-data/category';
 export * as Channel from '@commercetools-test-data/channel';
-export * as Commons from '@commercetools-test-data/commons';
-export * as Core from '@commercetools-test-data/core';
 export * as Customer from '@commercetools-test-data/customer';
 export * as Review from '@commercetools-test-data/review';
 export * as TaxCategory from '@commercetools-test-data/tax-category';
