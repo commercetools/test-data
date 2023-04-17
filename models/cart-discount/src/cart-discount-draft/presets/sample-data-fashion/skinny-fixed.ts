38c6e01cc489893b5d5d9c53860f4b8757872d84
jaikamat <jaipkamat@gmail.com>
2023-04-17 09:20:50 -0400

    chore(test-data): create skinnyFixed cart discount and integrate in discount code

---
+++ models/cart-discount/src/cart-discount-draft/presets/sample-data-fashion/skinny-fixed.ts
@@ -0,0 +1,39 @@
+import * as CartDiscountValueFixed from '@commercetools-test-data/cart-discount-value-fixed';
+import * as CentPrecisionMoney from '@commercetools-test-data/cent-precision-money';
+import { LocalizedString } from '@commercetools-test-data/commons';
+import * as CartDiscountDraft from '../..';
+import { TCartDiscountDraftBuilder } from '../../../types';
+
+const skinnyFixed = (): TCartDiscountDraftBuilder =>
+  CartDiscountDraft.presets
+    .empty()
+    .value(
+      CartDiscountValueFixed.random()
+        .type('fixed')
+        .money(
+          CentPrecisionMoney.random()
+            .currencyCode('EUR')
+            .centAmount(2500)
+            .fractionDigits(2)
+        )
+    )
+    .cartPredicate('1 = 1')
+    // TODO: create `target` model
+    .target(undefined)
+    .name(
+      LocalizedString.presets
+        .empty()
+        ['en-US']('Save on Skinny Jeans - 25 EUR per pair')
+    )
+    .description(
+      LocalizedString.presets
+        .empty()
+        ['en-US']('Save on Skinny Jeans at 25EUR per pair')
+    )
+    .stackingMode('Stacking')
+    .isActive(true)
+    .requiresDiscountCode(false)
+    .sortOrder('0.6')
+    .key('SkinnyFixed25');
+
+export default skinnyFixed;
