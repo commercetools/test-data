22efa3f488c31eae202e38db7172efe841b6e3f4
Tyler Ford <tyler.ford@commercetools.com>
2023-05-09 11:11:12 -0600

    fix(models): update entry

--- models/shipping-method/src/index.ts
+++ models/shipping-method/src/index.ts
@@ -1,6 +1,14 @@
 // Export types
 export * from './shipping-method/types';
+export * from './shipping-rate/types';
+export * from './zone-rate/types';
 
 // Export models
 export * as ShippingMethod from './shipping-method';
 export * as ShippingMethodDraft from './shipping-method/shipping-method-draft';
+
+export * as ShippingRate from './shipping-rate';
+export * as ShippingRateDraft from './shipping-rate/shipping-rate-draft';
+
+export * as ZoneRate from './zone-rate';
+export * as ZoneRateDraft from './zone-rate/zone-rate-draft';
