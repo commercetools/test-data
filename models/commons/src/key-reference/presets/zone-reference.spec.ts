aa4dc96e8ffd132e8303612d593f6a2da10f29fa
Tyler Ford <tyler.ford@commercetools.com>
2023-05-09 10:37:50 -0600

    fix(models): typo

--- models/commons/src/key-reference/presets/zone-reference.spec.ts
+++ models/commons/src/key-reference/presets/zone-reference.spec.ts
@@ -1,7 +1,7 @@
 import type { TKeyReference } from '../types';
 import zoneReference from './zone-reference';
 
-it('should build category reference', () => {
+it('should build zone reference', () => {
   const built = zoneReference().build<TKeyReference>();
   expect(built).toEqual({
     key: expect.any(String),
