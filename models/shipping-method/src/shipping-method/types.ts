4489003cdf1b4504a6f1cb036d393519e6b954b7
Tyler Ford <tyler.ford@commercetools.com>
2023-05-11 10:20:15 -0600

    fix: update types

--- models/shipping-method/src/shipping-method/types.ts
+++ models/shipping-method/src/shipping-method/types.ts
@@ -2,13 +2,16 @@ import type {
   ShippingMethod,
   ShippingMethodDraft,
   TaxCategory,
+  TaxCategoryReference,
 } from '@commercetools/platform-sdk';
+import { type TReferenceGraphql } from '@commercetools-test-data/commons';
 import type { TBuilder } from '@commercetools-test-data/core';
 
 export type TShippingMethod = ShippingMethod & { taxCategory: TaxCategory };
 export type TShippingMethodRest = ShippingMethod;
 export type TShippingMethodGraphql = TShippingMethod & {
   __typename: 'ShippingMethod';
+  taxCategoryRef: TReferenceGraphql;
 };
 
 export type TShippingMethodDraft = ShippingMethodDraft;
