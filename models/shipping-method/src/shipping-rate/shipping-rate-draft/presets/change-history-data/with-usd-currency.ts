92a4c3170e11b1139966156a8f92f610b84c656d
Tyler Ford <tyler.ford@commercetools.com>
2023-06-21 11:07:07 -0600

    fix: revert preset typo

--- models/shipping-method/src/shipping-rate/shipping-rate-draft/presets/change-history-data/with-usd-currency.ts
+++ models/shipping-method/src/shipping-rate/shipping-rate-draft/presets/change-history-data/with-usd-currency.ts
@@ -4,7 +4,7 @@ import * as ShippingRateDraft from '../../index';
 
 const usdCurrency = (): TShippingRateDraftBuilder =>
   ShippingRateDraft.random()
-    .price(Money.presets.changeHistoryData.withUsCurrencyCode())
-    .freeAbove(Money.presets.changeHistoryData.withUsCurrencyCode());
+    .price(Money.presets.changeHistoryData.withUsdCurrencyCode())
+    .freeAbove(Money.presets.changeHistoryData.withUsdCurrencyCode());
 
 export default usdCurrency;
