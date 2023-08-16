1dbb71be16b9e3fbd39d85044cd63fc70027c5d3
Tyler Ford <tyler.ford@commercetools.com>
2023-08-16 10:12:51 -0600

    chore(models): add draft and preset exports

--- models/payment/src/index.ts
+++ models/payment/src/index.ts
@@ -4,4 +4,7 @@ export * from './payment-status/types';
 
 // Export models
 export * as PaymentMethodInfo from './payment-method-info';
+export * as PaymentMethodInfoDraft from './payment-method-info/payment-method-info-draft';
+
 export * as PaymentStatus from './payment-status';
+export * as PaymentStatusDraft from './payment-status/payment-status-draft';
--- models/payment/src/payment-status/index.ts
+++ models/payment/src/payment-status/index.ts
@@ -1,3 +1,6 @@
+export * as PaymentStatusDraft from './payment-status-draft';
+
 export { default as random } from './builder';
 export { default as presets } from './presets';
+export { default as draftPresets } from './payment-status-draft/presets';
 export * from './types';
