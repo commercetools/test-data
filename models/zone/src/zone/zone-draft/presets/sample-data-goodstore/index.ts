fbc6317ab0c74e8b0ae544324a6745a079d98da7
Lidiya Dzhumayeva <81679574+lidiyacommercetools@users.noreply.github.com>
2023-09-13 16:37:49 -0400

    Create index.ts
    Creating index.ts for zone presets for good store data

---
+++ models/zone/src/zone/zone-draft/presets/sample-data-goodstore/index.ts
@@ -0,0 +1,9 @@
+import countryGermany from './country-germany';
+import countryGreatBritain from './country-greatbritain';
+
+const presets = {
+  countryGermany,
+  countryGreatBritain,
+};
+
+export default presets;
