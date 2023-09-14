66fa5e8450c43c9840a3df74f9de26c1ae156db3
Lidiya Dzhumayeva <81679574+lidiyacommercetools@users.noreply.github.com>
2023-09-14 15:51:18 -0400

    Uploaded the files into an incorrect folder
    Accidentally added the files into the wrong folder. Correcting for the error

--- models/zone/src/zone/zone-draft/presets/sample-data-goodstore/index.ts
+++
@@ -1,9 +0,0 @@
-import countryGermany from './country-germany';
-import countryUnitedKingdom from './country-unitedkingdom';
-
-const presets = {
-  countryGermany,
-  countryUnitedKingdom,
-};
-
-export default presets;
