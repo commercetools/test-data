7791341db9c46242e3bec1e296e0e4e4b758082f
Lidiya Dzhumayeva <81679574+lidiyacommercetools@users.noreply.github.com>
2023-09-14 19:39:02 -0400

    feat(zone): added presets for goodstore data (#341)
    * feat(zone): added presets for goodstore data
    
    * Create lovely-jars-trade.md

---
+++ models/zone/src/zone/zone-draft/presets/sample-data-goodstore/country-germany.spec.ts
@@ -0,0 +1,38 @@
+import { TZoneDraft, TZoneDraftGraphql } from '../../../types';
+import countryGermany from './country-germany';
+
+describe('with the preset `country germany`', () => {
+  it('should return a zone with name `Europe`', () => {
+    const zone = countryGermany().build<TZoneDraft>();
+
+    expect(zone).toMatchInlineSnapshot(`
+      {
+        "description": undefined,
+        "key": "europe",
+        "locations": [
+          {
+            "country": "DE",
+          },
+        ],
+        "name": "Europe",
+      }
+    `);
+  });
+
+  it('should return a zone with name `Europe` when built for GraphQL', () => {
+    const zoneGraphql = countryGermany().buildGraphql<TZoneDraftGraphql>();
+
+    expect(zoneGraphql).toMatchInlineSnapshot(`
+      {
+        "description": undefined,
+        "key": "europe",
+        "locations": [
+          {
+            "country": "DE",
+          },
+        ],
+        "name": "Europe",
+      }
+    `);
+  });
+});
