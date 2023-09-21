e016f3c7cc29b977895157f8affb1bf58508315d
Valorie Carli <valorie.carli@commercetools.com>
2023-09-21 13:10:50 +0200

    chore(customers): add goodstore presets for jennifer schmidt

--- models/commons/src/address/address-draft/presets/sample-data-goodstore/index.ts
+++ models/commons/src/address/address-draft/presets/sample-data-goodstore/index.ts
@@ -1,8 +1,10 @@
 import jenniferJonesAddress from './jennifer-jones-address';
+import jenniferSchmidtAddress from './jennifer-schmidt-address';
 import sebastianSmithAddress from './sebastian-smith-address';
 
 const presets = {
   jenniferJonesAddress,
+  jenniferSchmidtAddress,
   sebastianSmithAddress,
 };
 
