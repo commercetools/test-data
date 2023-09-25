88f40d8ddc57d10fde9eb4bf48089ee89e6401ca
jaikamat <jaipkamat@gmail.com>
2023-09-25 09:57:59 -0400

    chore(test-data): create goodstore cart discount and discount code presets

---
+++ models/discount-code/src/discount-code-draft/presets/sample-data-goodstore/furniture-bogo.ts
@@ -0,0 +1,19 @@
+import { LocalizedString, Reference } from '@commercetools-test-data/commons';
+import { TDiscountCodeDraftBuilder } from '../../../types';
+import * as DiscountCodeDraft from '../../index';
+
+const furnitureBogo = (cartDiscountId: string): TDiscountCodeDraftBuilder =>
+  DiscountCodeDraft.presets
+    .empty()
+    .code('BOGO')
+    .name(LocalizedString.presets.empty()['en-US']('BOGO')['en-GB']('BOGO'))
+    .description(LocalizedString.presets.empty())
+    .cartDiscounts([
+      Reference.random().id(cartDiscountId).typeId('cart-discount'),
+    ])
+    .isActive(true)
+    .maxApplications(1)
+    .maxApplicationsPerCustomer(1)
+    .groups([]);
+
+export default furnitureBogo;
