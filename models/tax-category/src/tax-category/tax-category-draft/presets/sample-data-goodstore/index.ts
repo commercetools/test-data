e50be2e8e9ef76de225bb9a9853eae289bb72336
Lidiya Dzhumayeva <81679574+lidiyacommercetools@users.noreply.github.com>
2023-09-26 09:52:36 -0400

    feat(tax category): edit of goodstore presets
    The tax category presets were updated to match these specs: https://mc.europe-west1.gcp.commercetools.com/thegoodstore/settings/project/taxes/ff653358-d76f-49a5-be78-995c5b028796

--- models/tax-category/src/tax-category/tax-category-draft/presets/sample-data-goodstore/index.ts
+++ models/tax-category/src/tax-category/tax-category-draft/presets/sample-data-goodstore/index.ts
@@ -1,7 +1,5 @@
-import EUTaxCategory from './eu-tax-category';
-import EU2TaxCategory from './eu2-tax-category';
-import USTaxCategory from './us-tax-category';
+import standardTaxCategory from './standard-tax-category';
 
-const presets = { EUTaxCategory, EU2TaxCategory, USTaxCategory };
+const presets = { standardTaxCategory };
 
 export default presets;
