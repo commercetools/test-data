850cfe04d45a630200d603d801cf6eb2d2820f50
jaikamat <jaipkamat@gmail.com>
2023-09-27 11:49:52 -0400

    chore(test-data): stub index

--- models/product/src/product/product-draft/presets/index.ts
+++ models/product/src/product/product-draft/presets/index.ts
@@ -1,6 +1,7 @@
 import empty from './empty';
 import sampleDataFashion from './sample-data-fashion';
+import sampleDataGoodStore from './sample-data-goodstore';
 
-const presets = { empty, sampleDataFashion };
+const presets = { empty, sampleDataFashion, sampleDataGoodStore };
 
 export default presets;
