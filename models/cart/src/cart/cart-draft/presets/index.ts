68dddaf9c7fd0c489a34f578771490d4ed85a19a
Lidiya Dzhumayeva <81679574+lidiyacommercetools@users.noreply.github.com>
2023-09-28 13:37:23 -0400

    message

--- models/cart/src/cart/cart-draft/presets/index.ts
+++ models/cart/src/cart/cart-draft/presets/index.ts
@@ -1,7 +1,13 @@
 import changeHistoryData from './change-history-data';
 import empty from './empty';
 import sampleDataFashion from './sample-data-fashion';
+import sampleDataGoodStore from './sample-data-goodstore';
 
-const presets = { changeHistoryData, sampleDataFashion, empty };
+const presets = {
+  changeHistoryData,
+  sampleDataFashion,
+  sampleDataGoodStore,
+  empty,
+};
 
 export default presets;
