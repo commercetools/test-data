da2d2972387096e025674c07e505bdae1a5b706e
Stephanie Sprinkle <stephanie.sprinkle@commercetools.com>
2023-10-02 17:15:02 -0400

    fix: import order

--- models/order/src/order-from-cart-draft/presets/sample-data-goodstore/index.ts
+++ models/order/src/order-from-cart-draft/presets/sample-data-goodstore/index.ts
@@ -1,15 +1,15 @@
 import jenniferJonesCart01 from './jennifer-jones-cart-01';
-import sebastianSmithCart01 from './sebastian-smith-cart-01';
 import jenniferSchmidtCart01 from './jennifer-schmidt-cart-01';
-import sebastianMuellerCart01 from './sebastian-mueller-cart-01';
 import sebastianFranklinCart01 from './sebastian-franklin-cart-01';
+import sebastianMuellerCart01 from './sebastian-mueller-cart-01';
+import sebastianSmithCart01 from './sebastian-smith-cart-01';
 
 const presets = {
   jenniferJonesCart01,
-  sebastianSmithCart01,
   jenniferSchmidtCart01,
-  sebastianMuellerCart01,
   sebastianFranklinCart01,
+  sebastianMuellerCart01,
+  sebastianSmithCart01,
 };
 
 export default presets;
