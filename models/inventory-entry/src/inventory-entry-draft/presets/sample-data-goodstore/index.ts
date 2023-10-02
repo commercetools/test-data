8573be79e016ba5d64eda455b445042e9535bc99
Tyler Ford <tyler.ford@commercetools.com>
2023-10-02 14:21:59 -0600

    feat(goodstore): add goodstore index file

---
+++ models/inventory-entry/src/inventory-entry-draft/presets/sample-data-goodstore/index.ts
@@ -0,0 +1,19 @@
+import skuAAR34 from './sku-AAR34';
+import skuADARM04 from './sku-ADARM04';
+import skuADCT01 from './sku-ADCT01';
+import skuADPC7 from './sku-ADPC7';
+import skuADPC09 from './sku-ADPC09';
+import skuALC01 from './sku-ALC01';
+
+// TODO: complete preset exports
+
+const presets = {
+  skuAAR34,
+  skuADARM04,
+  skuADCT01,
+  skuADPC7,
+  skuADPC09,
+  skuALC01,
+};
+
+export default presets;
