28cfc6200f61c7027237d53dca74a4cd5eabd9a4
tdeekens <tobias.deekens@commercetools.com>
2023-10-05 10:42:04 +0200

    refactor(ci): for aggregate release mode

---
+++ scripts/print_release_version.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Running "changeset version" to know the new release version
+pnpm changeset version &>/dev/null
+
+release_version=$(node -e "console.log(require('./core/package.json').version)")
+
+git reset --hard &>/dev/null
+
+echo "$release_version"
