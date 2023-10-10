a54a5d33af47cfb1b3bab30472e2fe3d9036325d
Tyler Ford <tylerford@gmail.com>
2023-10-10 12:50:42 -0600

    Fct payment presets fix (#412)
    * fix(paymentDrafts): rm unnecessary export
    
    * fix(paymentDrafts): changeset

--- models/payment/src/payment/index.ts
+++ models/payment/src/payment/index.ts
@@ -1,5 +1,4 @@
 export { default as random } from './builder';
 
 export { default as presets } from './presets';
-export { default as draftPresets } from './payment-draft/presets';
 export * from './types';
