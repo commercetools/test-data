e68b2abdbef2ddfc444ae7d05a223d7d906697b7
Jai Kamat <jaipkamat@gmail.com>
2023-10-11 11:16:44 -0700

    chore(test-data): activate goodstore discounts, alter product discounts (#415)

--- models/product-discount/src/product-discount/product-discount-draft/presets/sample-data-goodstore/discount-armchairs.ts
+++ models/product-discount/src/product-discount/product-discount-draft/presets/sample-data-goodstore/discount-armchairs.ts
@@ -1,26 +1,34 @@
+import {
+  CategoryDraft,
+  TCategoryDraft,
+} from '@commercetools-test-data/category';
 import { LocalizedString } from '@commercetools-test-data/commons';
 import { ProductDiscountValueRelativeDraft } from '../../../../index';
 import type { TProductDiscountDraftBuilder } from '../../../types';
 import * as ProductDiscountDraft from '../../index';
 
+const categoryTypeDraft = CategoryDraft.presets.sampleDataGoodStore
+  .armchairs()
+  .build<TCategoryDraft>();
+
 const discountNewArrivals = (): TProductDiscountDraftBuilder =>
   ProductDiscountDraft.presets
     .empty()
     .value(ProductDiscountValueRelativeDraft.random().permyriad(1500))
-    .predicate('categories.key contains "new-arrivals"')
+    .predicate(`categories.key contains "${categoryTypeDraft.key}"`)
     .name(
       LocalizedString.presets
         .empty()
-        ['en-US']('15% Off All New Arrivals')
-        ['en-GB']('15% Off All New Arrivals')
+        ['en-US']('15% Off All Armchairs')
+        ['en-GB']('15% Off All Armchairs')
     )
     .description(
       LocalizedString.presets
         .empty()
-        ['en-US']('15% Off All New Arrivals')
-        ['en-GB']('15% Off All New Arrivals')
+        ['en-US']('15% Off All Armchairs')
+        ['en-GB']('15% Off All Armchairs')
     )
-    .isActive(false)
+    .isActive(true)
     .sortOrder('0.67')
     .key('NewArrivals15pctOff');
 
