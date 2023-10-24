45abaa864c8471895dc63f37055e9fb699cd9aa9
Lidiya Dzhumayeva <81679574+lidiyacommercetools@users.noreply.github.com>
2023-10-24 14:54:55 -0400

    updated test

--- models/product/src/product-variant/product-variant-draft/presets/sample-data-goodstore/large-ceramic-plate-01.spec.ts
+++ models/product/src/product-variant/product-variant-draft/presets/sample-data-goodstore/large-ceramic-plate-01.spec.ts
@@ -5,7 +5,122 @@ describe(`with largeCeramicPlate01 preset`, () => {
   it(`should return a largeCeramicPlate01 preset`, () => {
     const largeCeramicPlate01Preset =
       largeCeramicPlate01().build<TProductVariantDraft>();
-    expect(largeCeramicPlate01Preset).toMatchInlineSnapshot();
+    expect(largeCeramicPlate01Preset).toMatchInlineSnapshot(`
+      {
+        "assets": undefined,
+        "attributes": [
+          {
+            "name": "color",
+            "value": {
+              "de-DE": "#d8deb1",
+              "en-GB": "#d8deb1",
+              "en-US": "#d8deb1",
+            },
+          },
+          {
+            "name": "colorlabel",
+            "value": {
+              "de-DE": "Light Olive ",
+              "en-GB": "Light Olive ",
+              "en-US": "Light Olive ",
+            },
+          },
+          {
+            "name": "productspec",
+            "value": {
+              "de-DE": "- Ceramic
+      - Sold as 1 piece
+      - Dishwasher safe",
+              "en-GB": "- Ceramic
+      - Sold as 1 piece
+      - Dishwasher safe
+      - Microwave safe",
+              "en-US": "- Ceramic
+      - Sold as 1 piece
+      - Dishwasher safe",
+            },
+          },
+          {
+            "name": "color-filter",
+            "value": {
+              "key": "#00FF00",
+              "label": {
+                "de-DE": "Grün",
+                "en-GB": "Green",
+                "en-US": "Green",
+              },
+            },
+          },
+        ],
+        "images": [
+          {
+            "dimensions": {
+              "h": 2700,
+              "w": 3000,
+            },
+            "label": undefined,
+            "url": "https://storage.googleapis.com/merchant-center-europe/sample-data/goodstore/Large_Ceramic_Plate-1.1.jpeg",
+          },
+          {
+            "dimensions": {
+              "h": 2700,
+              "w": 3000,
+            },
+            "label": undefined,
+            "url": "https://storage.googleapis.com/merchant-center-europe/sample-data/goodstore/Large_Ceramic_Plate-1.2.jpeg",
+          },
+        ],
+        "key": undefined,
+        "prices": [
+          {
+            "channel": undefined,
+            "country": "DE",
+            "custom": undefined,
+            "customerGroup": undefined,
+            "discounted": undefined,
+            "key": undefined,
+            "tiers": undefined,
+            "validFrom": undefined,
+            "validUntil": undefined,
+            "value": {
+              "centAmount": 399,
+              "currencyCode": "EUR",
+            },
+          },
+          {
+            "channel": undefined,
+            "country": "GB",
+            "custom": undefined,
+            "customerGroup": undefined,
+            "discounted": undefined,
+            "key": undefined,
+            "tiers": undefined,
+            "validFrom": undefined,
+            "validUntil": undefined,
+            "value": {
+              "centAmount": 399,
+              "currencyCode": "GBP",
+            },
+          },
+          {
+            "channel": undefined,
+            "country": "US",
+            "custom": undefined,
+            "customerGroup": undefined,
+            "discounted": undefined,
+            "key": undefined,
+            "tiers": undefined,
+            "validFrom": undefined,
+            "validUntil": undefined,
+            "value": {
+              "centAmount": 399,
+              "currencyCode": "USD",
+            },
+          },
+        ],
+        "sku": "LCP-01",
+      }
+    `);
   });
 
   it(`should return a largeCeramicPlate01 preset when built for graphql`, () => {
@@ -39,7 +154,7 @@ describe(`with largeCeramicPlate01 preset`, () => {
               "width": 3000,
             },
             "label": undefined,
-            "url": "https://2eca75039cf911b9bbe5-79bfd3e36f011d786971804e873c4354.ssl.cf3.rackcdn.com/AdobeStock_301934007-L_4CrBFU.jpeg",
+            "url": "https://storage.googleapis.com/merchant-center-europe/sample-data/goodstore/Large_Ceramic_Plate-1.1.jpeg",
           },
           {
             "dimensions": {
@@ -47,7 +162,7 @@ describe(`with largeCeramicPlate01 preset`, () => {
               "width": 3000,
             },
             "label": undefined,
-            "url": "https://2eca75039cf911b9bbe5-79bfd3e36f011d786971804e873c4354.ssl.cf3.rackcdn.com/AdobeStock_301935413-qxOvi5pU.jpeg",
+            "url": "https://storage.googleapis.com/merchant-center-europe/sample-data/goodstore/Large_Ceramic_Plate-1.2.jpeg",
           },
         ],
         "key": undefined,
