bfe83afd9e211b44c809961ea6884671554469f5
Kacper Krzywiec <krzywiec@gmail.com>
2023-11-20 12:24:21 +0100

    refactor: cleanup

--- models/custom-view/src/custom-view-installation/index.ts
+++ models/custom-view/src/custom-view-installation/index.ts
@@ -1,3 +1,2 @@
 // Export models
 export * as RestrictedCustomViewInstallationForOrganization from './restricted-custom-view-installation-for-organization';
-export * as CustomViewInstallationPermission from '../custom-view-installation-permission';
