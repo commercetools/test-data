72de2a414a68d2c7a17bfd31935fa629fe5e3436
Christian Moll <christian.moll@commercetools.com>
2023-11-29 17:32:41 +0100

    fix(business-unit): fix types

--- models/business-unit/src/types.ts
+++ models/business-unit/src/types.ts
@@ -1,6 +1,7 @@
 import type {
   BusinessUnit,
   BusinessUnitDraft,
+  Store,
 } from '@commercetools/platform-sdk';
 import type { TBuilder } from '@commercetools-test-data/core';
 
@@ -19,5 +20,7 @@ export type TBusinessUnitBuilder = TBuilder<TBusinessUnit>;
 export type TCreateBusinessUnitBuilder = () => TBusinessUnitBuilder;
 //BusinessUnitGraphql is only scaffolding at this time
 export type TBusinessUnitGraphql = TBusinessUnit & {
+  ancestors: Array<TBusinessUnitGraphql>;
+  inheritedStores: Array<Store> | null;
   __typename: 'BusinessUnit';
 };
