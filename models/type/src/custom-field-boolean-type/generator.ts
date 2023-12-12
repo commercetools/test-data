f06a36fcda705460fabbdde7c71dab82c1805004
Jonathan Creasman <jonathan@Jons-MBP.lan>
2023-12-12 14:40:59 -0500

    feat(type-model): add CustomFieldBooleanType model

---
+++ models/type/src/custom-field-boolean-type/generator.ts
@@ -0,0 +1,10 @@
+import { Generator } from '@commercetools-test-data/core';
+import { TCustomFieldBooleanType } from './types';
+
+// https://docs.commercetools.com/api/projects/types#customfieldbooleantype
+
+export const generator = Generator<TCustomFieldBooleanType>({
+  fields: {
+    name: 'Boolean',
+  },
+});
