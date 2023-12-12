3398756acbb6c97575ae9434da0824a338e78464
Sarah Brolley <sarahbrolley@gmail.com>
2023-12-12 16:59:46 -0500

    fix(inventory): change quantityOnStock and restockableInDays to not generate a data value that could be the same

--- models/inventory-entry/src/inventory-entry-draft/generator.ts
+++ models/inventory-entry/src/inventory-entry-draft/generator.ts
@@ -11,8 +11,8 @@ const generator = Generator<TInventoryEntryDraft>({
   fields: {
     key: fake((f) => f.lorem.slug(2)),
     sku: fake((f) => f.lorem.words()),
-    quantityOnStock: fake((f) => f.number.int({ min: 10, max: 30 })),
-    restockableInDays: fake((f) => f.number.int({ min: 1, max: 20 })),
+    quantityOnStock: fake((f) => f.number.int({ min: 11, max: 30 })),
+    restockableInDays: fake((f) => f.number.int({ min: 1, max: 10 })),
     expectedDelivery: fake(getFutureDate),
     supplyChannel: fake(() => Reference.random().typeId('channel')),
     custom: null,
