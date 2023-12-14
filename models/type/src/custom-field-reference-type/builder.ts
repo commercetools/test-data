31b4fcebb7e5798d32712a1ff42b68193e4b4977
Jonathan Creasman <jonathan@Jons-MBP.lan>
2023-12-14 12:52:31 -0500

    feat(type-model): add CustomFieldReferenceType

---
+++ models/type/src/custom-field-reference-type/builder.ts
@@ -0,0 +1,15 @@
+import { Builder } from '@commercetools-test-data/core';
+import { generator } from './generator';
+import transformers from './transformers';
+import {
+  TCustomFieldReferenceType,
+  TCreateCustomFieldReferenceTypeBuilder,
+} from './types';
+
+const Model: TCreateCustomFieldReferenceTypeBuilder = () =>
+  Builder<TCustomFieldReferenceType>({
+    generator,
+    transformers,
+  });
+
+export default Model;
