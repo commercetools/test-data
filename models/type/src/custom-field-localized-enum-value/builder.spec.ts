ff8fdd02a3e5dd25caf882ddb2d8c6d6d9e87346
Jonathan Creasman <jonathan@Jons-MBP.lan>
2023-12-14 15:12:32 -0500

    feat(type-model): add CustomFieldLocalizedEnumValue

---
+++ models/type/src/custom-field-localized-enum-value/builder.spec.ts
@@ -0,0 +1,67 @@
+/* eslint-disable jest/no-disabled-tests */
+/* eslint-disable jest/valid-title */
+import { createBuilderSpec } from '@commercetools-test-data/core/test-utils';
+import {
+  type TCustomFieldLocalizedEnumValue,
+  TCustomFieldLocalizedEnumValueGraphql,
+} from './types';
+import * as CustomFieldLocalizedEnumValue from './index';
+
+describe('builder', () => {
+  it(
+    ...createBuilderSpec<
+      TCustomFieldLocalizedEnumValue,
+      TCustomFieldLocalizedEnumValue
+    >(
+      'default',
+      CustomFieldLocalizedEnumValue.random(),
+      expect.objectContaining({
+        key: expect.any(String),
+        label: expect.objectContaining({
+          de: expect.any(String),
+          en: expect.any(String),
+          fr: expect.any(String),
+        }),
+      })
+    )
+  );
+
+  it(
+    ...createBuilderSpec<
+      TCustomFieldLocalizedEnumValue,
+      TCustomFieldLocalizedEnumValue
+    >(
+      'rest',
+      CustomFieldLocalizedEnumValue.random(),
+      expect.objectContaining({
+        key: expect.any(String),
+        label: expect.objectContaining({
+          de: expect.any(String),
+          en: expect.any(String),
+          fr: expect.any(String),
+        }),
+      })
+    )
+  );
+
+  it(
+    ...createBuilderSpec<
+      TCustomFieldLocalizedEnumValue,
+      TCustomFieldLocalizedEnumValueGraphql
+    >(
+      'graphql',
+      CustomFieldLocalizedEnumValue.random(),
+      expect.objectContaining({
+        key: expect.any(String),
+        labelAllLocales: expect.arrayContaining([
+          expect.objectContaining({
+            locale: 'en',
+            value: expect.any(String),
+            __typename: 'LocalizedString',
+          }),
+        ]),
+        __typename: 'LocalizableEnumValueType',
+      })
+    )
+  );
+});
---
+++ models/type/src/custom-field-localized-enum-value/transformers.ts
@@ -0,0 +1,30 @@
+import { LocalizedString } from '@commercetools-test-data/commons';
+import { Transformer } from '@commercetools-test-data/core';
+import {
+  type TCustomFieldLocalizedEnumValue,
+  type TCustomFieldLocalizedEnumValueGraphql,
+} from './types';
+
+const transformers = {
+  default: Transformer<
+    TCustomFieldLocalizedEnumValue,
+    TCustomFieldLocalizedEnumValue
+  >('default', { buildFields: ['label'] }),
+  rest: Transformer<
+    TCustomFieldLocalizedEnumValue,
+    TCustomFieldLocalizedEnumValue
+  >('rest', { buildFields: ['label'] }),
+  graphql: Transformer<
+    TCustomFieldLocalizedEnumValue,
+    TCustomFieldLocalizedEnumValueGraphql
+  >('graphql', {
+    buildFields: [],
+    removeFields: ['label'],
+    addFields: ({ fields }) => ({
+      labelAllLocales: LocalizedString.toLocalizedField(fields.label),
+      __typename: 'LocalizableEnumValueType',
+    }),
+  }),
+};
+
+export default transformers;
