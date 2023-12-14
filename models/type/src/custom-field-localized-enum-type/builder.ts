8b064a021c4cca74a38c093ed8489b9c58047fb2
Jonathan Creasman <jonathan@Jons-MBP.lan>
2023-12-14 15:28:51 -0500

    feat(type-model): add CustomFieldLocalizedEnumType

---
+++ models/type/src/custom-field-localized-enum-type/builder.ts
@@ -0,0 +1,15 @@
+import { Builder } from '@commercetools-test-data/core';
+import generator from './generator';
+import transformers from './transformers';
+import {
+  TCustomFieldLocalizedEnumType,
+  TCreateCustomFieldLocalizedEnumTypeBuilder,
+} from './types';
+
+const Model: TCreateCustomFieldLocalizedEnumTypeBuilder = () =>
+  Builder<TCustomFieldLocalizedEnumType>({
+    generator,
+    transformers,
+  });
+
+export default Model;
---
+++ models/type/src/custom-field-localized-enum-type/transformers.ts
@@ -0,0 +1,35 @@
+import { buildField, Transformer } from '@commercetools-test-data/core';
+import { TCustomFieldLocalizedEnumValueGraphql } from '../custom-field-localized-enum-value';
+import {
+  type TCustomFieldLocalizedEnumType,
+  type TCustomFieldLocalizedEnumTypeGraphql,
+} from './types';
+
+const transformers = {
+  default: Transformer<
+    TCustomFieldLocalizedEnumType,
+    TCustomFieldLocalizedEnumType
+  >('default', { buildFields: ['values'] }),
+  rest: Transformer<
+    TCustomFieldLocalizedEnumType,
+    TCustomFieldLocalizedEnumType
+  >('rest', { buildFields: ['values'] }),
+  graphql: Transformer<
+    TCustomFieldLocalizedEnumType,
+    TCustomFieldLocalizedEnumTypeGraphql
+  >('graphql', {
+    buildFields: [],
+    replaceFields: ({ fields }) => ({
+      ...fields,
+      values: {
+        results: fields.values!.map((value) =>
+          buildField(value, 'graphql')
+        ) as unknown as Array<TCustomFieldLocalizedEnumValueGraphql>,
+        __typename: 'LocalizableEnumValueTypeResult',
+      },
+      __typename: 'LocalizableEnumCustomFieldType',
+    }),
+  }),
+};
+
+export default transformers;
