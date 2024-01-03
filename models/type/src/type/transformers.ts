59f6864f30d19f466f4f88b1c6ffed6f84ac6ee2
Jonathan Creasman <jonathan@Jons-MacBook-Pro.local>
2024-01-03 10:06:58 -0500

    feat(type-model): update transformer files

--- models/type/src/type/transformers.ts
+++ models/type/src/type/transformers.ts
@@ -4,14 +4,32 @@ import type { TType, TTypeGraphql } from './types';
 
 const transformers = {
   default: Transformer<TType, TType>('default', {
-    buildFields: ['name', 'description', 'createdBy', 'lastModifiedBy'],
+    buildFields: [
+      'name',
+      'description',
+      'createdBy',
+      'lastModifiedBy',
+      'fieldDefinitions',
+    ],
   }),
   rest: Transformer<TType, TType>('rest', {
-    buildFields: ['name', 'description', 'createdBy', 'lastModifiedBy'],
+    buildFields: [
+      'name',
+      'description',
+      'createdBy',
+      'lastModifiedBy',
+      'fieldDefinitions',
+    ],
   }),
   // Note that the Type graphql is provided as scaffolding only and may not be complete at this time.
   graphql: Transformer<TType, TTypeGraphql>('graphql', {
-    buildFields: ['name', 'description', 'createdBy', 'lastModifiedBy'],
+    buildFields: [
+      'name',
+      'description',
+      'createdBy',
+      'lastModifiedBy',
+      'fieldDefinitions',
+    ],
     addFields: ({ fields }) => {
       const nameAllLocales = LocalizedString.toLocalizedField(fields.name);
       const descriptionAllLocales = LocalizedString.toLocalizedField(
--- models/type/src/type/type-draft/transformers.ts
+++ models/type/src/type/type-draft/transformers.ts
@@ -3,14 +3,14 @@ import type { TTypeDraft, TTypeDraftGraphql } from '../types';
 
 const transformers = {
   default: Transformer<TTypeDraft, TTypeDraft>('default', {
-    buildFields: ['name', 'description'],
+    buildFields: ['name', 'description', 'fieldDefinitions'],
   }),
   rest: Transformer<TTypeDraft, TTypeDraft>('rest', {
-    buildFields: ['name', 'description'],
+    buildFields: ['name', 'description', 'fieldDefinitions'],
   }),
   // Note that the TypeDraft graphql is provided as scaffolding only and may not be complete at this time.
   graphql: Transformer<TTypeDraft, TTypeDraftGraphql>('graphql', {
-    buildFields: ['name', 'description'],
+    buildFields: ['name', 'description', 'fieldDefinitions'],
   }),
 };
 
