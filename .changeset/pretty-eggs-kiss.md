6da8af0291c9769f9d2654408e16ddb8cf728217
Jai Kamat <jaipkamat@gmail.com>
2024-02-16 12:16:04 -0800

    chore(td): create staged quote KeyReferenceDraft preset (#484)
    * chore(td): create staged quote KeyReferenceDraft preset
    
    * chore(td): export

---
+++ .changeset/pretty-eggs-kiss.md
@@ -0,0 +1,5 @@
+---
+'@commercetools-test-data/commons': patch
+---
+
+Create staged quote KeyReferenceDraft preset
--- models/commons/src/key-reference/key-reference-draft/presets/tax-category-reference.spec.ts
+++ models/commons/src/key-reference/key-reference-draft/presets/tax-category-reference.spec.ts
@@ -1,7 +1,7 @@
 import type { TKeyReferenceDraft } from '../../types';
 import taxCategoryReference from './tax-category-reference';
 
-it('should build category reference', () => {
+it('should build tax category reference', () => {
   const built = taxCategoryReference().build<TKeyReferenceDraft>();
   expect(built).toEqual({
     key: expect.any(String),
