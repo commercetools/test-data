f22dfa3e7c3aa99f2c518db6bf63d72715eb6aaa
Tyler Ford <tylerford@gmail.com>
2024-02-22 13:41:49 -0700

    feat(attribute-plain-enum-value): add attribute-plain-enum-value-draft (#486)
    * feat(attribute-plain-enum-value): add attribute-plain-enum-value-draft
    
    * fix(attribute-plain-enum-value-draft): fix copy paste error and add changeset
    
    * fix(attribute-plain-enum-value-draft): simplify graphql type

---
+++ .changeset/light-moons-divide.md
@@ -0,0 +1,5 @@
+---
+'@commercetools-test-data/product-type': minor
+---
+
+Adds AttributePlainEnumValueDraft model.
--- models/product-type/src/attribute-plain-enum-value/types.ts
+++ models/product-type/src/attribute-plain-enum-value/types.ts
@@ -2,12 +2,19 @@ import { AttributePlainEnumValue } from '@commercetools/platform-sdk';
 import type { TBuilder } from '@commercetools-test-data/core';
 
 export type TAttributePlainEnumValue = AttributePlainEnumValue;
+export type TAttributePlainEnumValueDraft = AttributePlainEnumValue;
 
 export type TAttributePlainEnumValueGraphql = TAttributePlainEnumValue & {
   __typename: 'AttributePlainEnumValue';
 };
+export type TAttributePlainEnumValueDraftGraphql = AttributePlainEnumValue;
 
 export type TAttributePlainEnumValueBuilder =
   TBuilder<TAttributePlainEnumValue>;
 export type TCreateAttributePlainEnumValueBuilder =
   () => TAttributePlainEnumValueBuilder;
+
+export type TAttributePlainEnumValueDraftBuilder =
+  TBuilder<TAttributePlainEnumValueDraft>;
+export type TCreateAttributePlainEnumValueDraftBuilder =
+  () => TAttributePlainEnumValueDraftBuilder;
