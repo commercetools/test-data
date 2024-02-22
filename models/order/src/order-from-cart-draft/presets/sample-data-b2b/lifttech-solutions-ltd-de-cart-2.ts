0dc4b075171b5dca0685baf15d860a55ae231fcb
Stephanie Sprinkle <stephanie.sprinkle@commercetools.com>
2024-02-22 15:49:14 -0500

    feat(presets/b2b): orders from carts

---
+++ models/order/src/order-from-cart-draft/presets/sample-data-b2b/lifttech-solutions-ltd-de-cart-2.ts
@@ -0,0 +1,23 @@
+import { CartDraft } from '@commercetools-test-data/cart';
+import type { TCartDraft } from '@commercetools-test-data/cart';
+import { KeyReferenceDraft } from '@commercetools-test-data/commons';
+import { constants } from '../../../index';
+import { TOrderFromCartDraftBuilder } from '../../../types';
+import * as OrderFromCartDraft from '../../index';
+
+const cart = CartDraft.presets.sampleDataB2B
+  .lifttechSolutionsLtdDeCart2()
+  .build<TCartDraft>();
+
+const lifttechSolutionsLtdDeCartCart2 = (
+  versionNumber: number
+): TOrderFromCartDraftBuilder =>
+  OrderFromCartDraft.presets
+    .empty()
+    .version(versionNumber)
+    .cart(KeyReferenceDraft.random().key(cart.key!).typeId('cart'))
+    .orderState(constants.orderState.Confirmed)
+    .paymentState(constants.paymentState.BalanceDue)
+    .shipmentState(constants.shipmentState.Backorder);
+
+export default lifttechSolutionsLtdDeCartCart2;
---
+++ models/order/src/order-from-cart-draft/presets/sample-data-b2b/lifttech-solutions-ltd-fr-cart-2.spec.ts
@@ -0,0 +1,46 @@
+import type { TOrderFromCartDraft } from '../../../types';
+import lifttechSolutionsLtdFrCart2 from './lifttech-solutions-ltd-fr-cart-2';
+
+describe(`with lifttechSolutionsLtdFrCart2 preset`, () => {
+  it(`should return a lifttechSolutionsLtdFrCart2 preset`, () => {
+    const lifttechSolutionsLtdFrCart2Preset =
+      lifttechSolutionsLtdFrCart2(1).build<TOrderFromCartDraft>();
+    expect(lifttechSolutionsLtdFrCart2Preset).toMatchInlineSnapshot(`
+      {
+        "cart": {
+          "key": "lifttech-solutions-ltd-fr-cart-2",
+          "typeId": "cart",
+        },
+        "custom": undefined,
+        "orderNumber": undefined,
+        "orderState": "Complete",
+        "paymentState": "Paid",
+        "purchaseOrderNumber": undefined,
+        "shipmentState": "Delivered",
+        "state": undefined,
+        "version": 1,
+      }
+    `);
+  });
+
+  it(`should return a lifttechSolutionsLtdFrCart2 preset when built for graphql`, () => {
+    const lifttechSolutionsLtdFrCart2PresetGraphql =
+      lifttechSolutionsLtdFrCart2(1).buildGraphql<TOrderFromCartDraft>();
+    expect(lifttechSolutionsLtdFrCart2PresetGraphql).toMatchInlineSnapshot(`
+      {
+        "cart": {
+          "key": "lifttech-solutions-ltd-fr-cart-2",
+          "typeId": "cart",
+        },
+        "custom": undefined,
+        "orderNumber": undefined,
+        "orderState": "Complete",
+        "paymentState": "Paid",
+        "purchaseOrderNumber": undefined,
+        "shipmentState": "Delivered",
+        "state": undefined,
+        "version": 1,
+      }
+    `);
+  });
+});
