44a0509746e49836f2649115c44759d8b9d84c15
Valorie <47182700+valoriecarli@users.noreply.github.com>
2024-03-08 08:04:40 -0600

    PANGOLIN-35778- CH presets (#526)
    * chore(prod-discount): add CH presets
    
    * chore(changeset): add update to changelog
    
    * fix(type): update expected type

--- models/product-discount/src/product-discount-value-absolute/index.ts
+++ models/product-discount/src/product-discount-value-absolute/index.ts
@@ -2,5 +2,5 @@ export * as ProductDiscountValueAbsoluteDraft from './product-discount-value-abs
 
 export { default as random } from './builder';
 export { default as presets } from './presets';
-
+export { default as draftPresets } from './product-discount-value-absolute-draft/presets';
 export * from './types';
