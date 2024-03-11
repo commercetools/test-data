fa9d3276558cd5c5ffd5bcc1678daaa3f53e8caf
Nicolas Lucero <nicolas.lucero-ext@commercetools.com>
2024-03-11 23:28:45 +0100

    fix(b2b): remove product variants

--- models/product/src/product-variant/product-variant-draft/presets/sample-data-b2b/index.ts
+++ models/product/src/product-variant/product-variant-draft/presets/sample-data-b2b/index.ts
@@ -1,60 +1,24 @@
 import a789Bc01 from './a-789-bc-01';
 import a789Bc02 from './a-789-bc-02';
 import a789Bc03 from './a-789-bc-03';
-import aa123Qr01 from './aa-123-qr-01';
-import aa123Qr02 from './aa-123-qr-02';
-import aa123Qr03 from './aa-123-qr-03';
-import aaa789Uv01 from './aaa-789-uv-01';
-import aaa789Uv02 from './aaa-789-uv-02';
-import aaa789Uv03 from './aaa-789-uv-03';
 import airFilter01 from './air-filter-01';
 import alternator01 from './alternator-01';
 import b456St01 from './b-456-st-01';
 import b456St02 from './b-456-st-02';
 import b456St03 from './b-456-st-03';
-import bb456St01 from './bb-456-st-01';
-import bb456St02 from './bb-456-st-02';
-import bb456St03 from './bb-456-st-03';
-import bbb234Wx01 from './bbb-234-wx-01';
-import bbb234Wx02 from './bbb-234-wx-02';
-import bbb234Wx03 from './bbb-234-wx-03';
 import brakePadSet01 from './brake-pad-set-01';
 import c789Uv01 from './c-789-uv-01';
 import c789Uv02 from './c-789-uv-02';
 import c789Uv03 from './c-789-uv-03';
-import cc789Uv01 from './cc-789-uv-01';
-import cc789Uv02 from './cc-789-uv-02';
-import cc789Uv03 from './cc-789-uv-03';
-import ccc567Yz01 from './ccc-567-yz-01';
-import ccc567Yz02 from './ccc-567-yz-02';
-import ccc567Yz03 from './ccc-567-yz-03';
 import d234Wx01 from './d-234-wx-01';
 import d234Wx02 from './d-234-wx-02';
 import d234Wx03 from './d-234-wx-03';
-import dd234Wx01 from './dd-234-wx-01';
-import dd234Wx02 from './dd-234-wx-02';
-import dd234Wx03 from './dd-234-wx-03';
-import ddd890Op01 from './ddd-890-op-01';
-import ddd890Op02 from './ddd-890-op-02';
-import ddd890Op03 from './ddd-890-op-03';
 import e567Yz01 from './e-567-yz-01';
 import e567Yz02 from './e-567-yz-02';
 import e567Yz03 from './e-567-yz-03';
-import ee567Yz01 from './ee-567-yz-01';
-import ee567Yz02 from './ee-567-yz-02';
-import ee567Yz03 from './ee-567-yz-03';
-import eee123Qr01 from './eee-123-qr-01';
-import eee123Qr02 from './eee-123-qr-02';
-import eee123Qr03 from './eee-123-qr-03';
 import f890Op01 from './f-890-op-01';
 import f890Op02 from './f-890-op-02';
 import f890Op03 from './f-890-op-03';
-import ff890Op01 from './ff-890-op-01';
-import ff890Op02 from './ff-890-op-02';
-import ff890Op03 from './ff-890-op-03';
-import fff456St01 from './fff-456-st-01';
-import fff456St02 from './fff-456-st-02';
-import fff456St03 from './fff-456-st-03';
 import fuelFilter01 from './fuel-filter-01';
 import g123Qr01 from './g-123-qr-01';
 import g123Qr02 from './g-123-qr-02';
@@ -62,18 +26,12 @@ import g123Qr03 from './g-123-qr-03';
 import gg123Qr01 from './gg-123-qr-01';
 import gg123Qr02 from './gg-123-qr-02';
 import gg123Qr03 from './gg-123-qr-03';
-import ggg789Uv01 from './ggg-789-uv-01';
-import ggg789Uv02 from './ggg-789-uv-02';
-import ggg789Uv03 from './ggg-789-uv-03';
 import h456St01 from './h-456-st-01';
 import h456St02 from './h-456-st-02';
 import h456St03 from './h-456-st-03';
 import hh456St01 from './hh-456-st-01';
 import hh456St02 from './hh-456-st-02';
 import hh456St03 from './hh-456-st-03';
-import hhh234Wx01 from './hhh-234-wx-01';
-import hhh234Wx02 from './hhh-234-wx-02';
-import hhh234Wx03 from './hhh-234-wx-03';
 import hydraulicHose01 from './hydraulic-hose-01';
 import i789Uv01 from './i-789-uv-01';
 import i789Uv02 from './i-789-uv-02';
@@ -81,18 +39,12 @@ import i789Uv03 from './i-789-uv-03';
 import ii789Uv01 from './ii-789-uv-01';
 import ii789Uv02 from './ii-789-uv-02';
 import ii789Uv03 from './ii-789-uv-03';
-import iii567Yz01 from './iii-567-yz-01';
-import iii567Yz02 from './iii-567-yz-02';
-import iii567Yz03 from './iii-567-yz-03';
 import j234Wx01 from './j-234-wx-01';
 import j234Wx02 from './j-234-wx-02';
 import j234Wx03 from './j-234-wx-03';
 import jj234Wx01 from './jj-234-wx-01';
 import jj234Wx02 from './jj-234-wx-02';
 import jj234Wx03 from './jj-234-wx-03';
-import jjj890Op01 from './jjj-890-op-01';
-import jjj890Op02 from './jjj-890-op-02';
-import jjj890Op03 from './jjj-890-op-03';
 import k567Yz01 from './k-567-yz-01';
 import k567Yz02 from './k-567-yz-02';
 import k567Yz03 from './k-567-yz-03';
@@ -106,9 +58,6 @@ import ledWorkLight01 from './led-work-light-01';
 import ll890Op01 from './ll-890-op-01';
 import ll890Op02 from './ll-890-op-02';
 import ll890Op03 from './ll-890-op-03';
-import m890Op01 from './m-890-op-01';
-import m890Op02 from './m-890-op-02';
-import m890Op03 from './m-890-op-03';
 import mm123Qr01 from './mm-123-qr-01';
 import mm123Qr02 from './mm-123-qr-02';
 import mm123Qr03 from './mm-123-qr-03';
@@ -122,9 +71,6 @@ import oilFilter01 from './oil-filter-01';
 import oo789Uv01 from './oo-789-uv-01';
 import oo789Uv02 from './oo-789-uv-02';
 import oo789Uv03 from './oo-789-uv-03';
-import p234Qw01 from './p-234-qw-01';
-import p234Qw02 from './p-234-qw-02';
-import p234Qw03 from './p-234-qw-03';
 import pinAndBushingKit01 from './pin-and-bushing-kit-01';
 import pistonRingSet01 from './piston-ring-set-01';
 import pneumaticTire01 from './pneumatic-tire-01';
@@ -140,9 +86,6 @@ import r123Ts03 from './r-123-ts-03';
 import rr890Op01 from './rr-890-op-01';
 import rr890Op02 from './rr-890-op-02';
 import rr890Op03 from './rr-890-op-03';
-import s567Uv01 from './s-567-uv-01';
-import s567Uv02 from './s-567-uv-02';
-import s567Uv03 from './s-567-uv-03';
 import ss123Qr01 from './ss-123-qr-01';
 import ss123Qr02 from './ss-123-qr-02';
 import ss123Qr03 from './ss-123-qr-03';
@@ -181,15 +124,9 @@ import xx890Op03 from './xx-890-op-03';
 import y567Yz01 from './y-567-yz-01';
 import y567Yz02 from './y-567-yz-02';
 import y567Yz03 from './y-567-yz-03';
-import yy123Qr01 from './yy-123-qr-01';
-import yy123Qr02 from './yy-123-qr-02';
-import yy123Qr03 from './yy-123-qr-03';
 import z890Op01 from './z-890-op-01';
 import z890Op02 from './z-890-op-02';
 import z890Op03 from './z-890-op-03';
-import zz456St01 from './zz-456-st-01';
-import zz456St02 from './zz-456-st-02';
-import zz456St03 from './zz-456-st-03';
 
 const presets = {
   hydraulicHose01,
@@ -213,15 +150,6 @@ const presets = {
   a789Bc01,
   a789Bc02,
   a789Bc03,
-  p234Qw01,
-  p234Qw02,
-  p234Qw03,
-  s567Uv01,
-  s567Uv02,
-  s567Uv03,
-  m890Op01,
-  m890Op02,
-  m890Op03,
   n123Qr01,
   n123Qr02,
   n123Qr03,
@@ -276,24 +204,6 @@ const presets = {
   z890Op01,
   z890Op02,
   z890Op03,
-  aa123Qr01,
-  aa123Qr02,
-  aa123Qr03,
-  bb456St01,
-  bb456St02,
-  bb456St03,
-  cc789Uv01,
-  cc789Uv02,
-  cc789Uv03,
-  dd234Wx01,
-  dd234Wx02,
-  dd234Wx03,
-  ee567Yz01,
-  ee567Yz02,
-  ee567Yz03,
-  ff890Op01,
-  ff890Op02,
-  ff890Op03,
   gg123Qr01,
   gg123Qr02,
   gg123Qr03,
@@ -348,42 +258,6 @@ const presets = {
   xx890Op01,
   xx890Op02,
   xx890Op03,
-  yy123Qr01,
-  yy123Qr02,
-  yy123Qr03,
-  zz456St01,
-  zz456St02,
-  zz456St03,
-  aaa789Uv01,
-  aaa789Uv02,
-  aaa789Uv03,
-  bbb234Wx01,
-  bbb234Wx02,
-  bbb234Wx03,
-  ccc567Yz01,
-  ccc567Yz02,
-  ccc567Yz03,
-  ddd890Op01,
-  ddd890Op02,
-  ddd890Op03,
-  eee123Qr01,
-  eee123Qr02,
-  eee123Qr03,
-  fff456St01,
-  fff456St02,
-  fff456St03,
-  ggg789Uv01,
-  ggg789Uv02,
-  ggg789Uv03,
-  hhh234Wx01,
-  hhh234Wx02,
-  hhh234Wx03,
-  iii567Yz01,
-  iii567Yz02,
-  iii567Yz03,
-  jjj890Op01,
-  jjj890Op02,
-  jjj890Op03,
 };
 
 export default presets;
