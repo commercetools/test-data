1e3070677bc86dab93517d262b893bd95b0810c3
Stephanie Sprinkle <stephanie.sprinkle@commercetools.com>
2024-03-18 11:49:57 -0400

    fix(presets/b2b): remove order from quote draft

--- models/order/src/order-from-quote-draft/presets/sample-data-b2b/index.ts
+++ models/order/src/order-from-quote-draft/presets/sample-data-b2b/index.ts
@@ -2,12 +2,10 @@ import eagleHeavyLiftTechnologiesIncUsQuote1 from './eagle-heavy-lift-technologi
 import libertyCraneSolutionsLlcUsQuote1 from './liberty-crane-solutions-llc-us-quote-1';
 import lifttechSolutionsLtdEsQuote1 from './lifttech-solutions-ltd-es-quote-1';
 import lifttechSolutionsLtdFrQuote1 from './lifttech-solutions-ltd-fr-quote-1';
-import lifttechSolutionsLtdQuote1 from './lifttech-solutions-ltd-quote-1';
 
 const presets = {
   eagleHeavyLiftTechnologiesIncUsQuote1,
   libertyCraneSolutionsLlcUsQuote1,
-  lifttechSolutionsLtdQuote1,
   lifttechSolutionsLtdFrQuote1,
   lifttechSolutionsLtdEsQuote1,
 };
