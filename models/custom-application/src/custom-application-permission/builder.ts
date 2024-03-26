d62a7120e23d0cf75a69edc3ec56073c213456df
Yemitan Isaiah Olurotimi <isaiah.yemitan@commercetools.com>
2024-03-26 15:26:26 +0100

    feat: add custom app permission models

---
+++ models/custom-application/src/custom-application-permission/builder.ts
@@ -0,0 +1,15 @@
+import { Builder } from '@commercetools-test-data/core';
+import generator from './generator';
+import transformers from './transformers';
+import type {
+  TCustomApplicationPermission,
+  TCreateCustomApplicationPermissionBuilder,
+} from './types';
+
+const Model: TCreateCustomApplicationPermissionBuilder = () =>
+  Builder<TCustomApplicationPermission>({
+    generator,
+    transformers,
+  });
+
+export default Model;
