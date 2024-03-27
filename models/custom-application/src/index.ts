2a075cbe9f07cc16baffa380fa1979ae5226ea0a
Yemitan Isaiah Olurotimi <isaiah.yemitan@commercetools.com>
2024-03-27 17:08:08 +0100

    feat: add index.ts for custom application model

---
+++ models/custom-application/src/index.ts
@@ -0,0 +1,19 @@
+import * as CustomApplication from './custom-application';
+import * as CustomApplicationDraft from './custom-application/custom-application-draft';
+import * as CustomApplicationInstallation from './custom-application-installation';
+import * as CustomApplicationInstallationPermission from './custom-application-installation-permission';
+import * as CustomApplicationMenuLink from './custom-application-menu-link';
+import * as CustomApplicationPermission from './custom-application-permission';
+import * as CustomApplicationPermissionDraft from './custom-application-permission-draft';
+import * as CustomApplicationSubmenuLink from './custom-application-sub-menu-link';
+
+export {
+  CustomApplication,
+  CustomApplicationDraft,
+  CustomApplicationInstallation,
+  CustomApplicationInstallationPermission,
+  CustomApplicationMenuLink,
+  CustomApplicationPermission,
+  CustomApplicationPermissionDraft,
+  CustomApplicationSubmenuLink,
+};
