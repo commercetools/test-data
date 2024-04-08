f3fb164e6661ec61571298509474b4c68392f607
Tyler Ford <tyler.ford@commercetools.com>
2024-04-08 14:49:24 -0600

    fix(shipping-rate): update tests to use snapshots

--- models/shipping-method/src/shipping-rate/shipping-rate-draft/presets/sample-data-goodstore/eur-50000.spec.ts
+++ models/shipping-method/src/shipping-rate/shipping-rate-draft/presets/sample-data-goodstore/eur-50000.spec.ts
@@ -8,19 +8,31 @@ describe('with eur50000 preset', () => {
   it('should return a eur50000 preset', () => {
     const eur50000Preset = eur50000().build<TShippingRateDraft>();
 
-    expect(eur50000Preset).toMatchObject({
-      price: { centAmount: 50000, currencyCode: 'EUR' },
-      tiers: [],
-    });
+    expect(eur50000Preset).toMatchInlineSnapshot(`
+      {
+        "freeAbove": undefined,
+        "price": {
+          "centAmount": 50000,
+          "currencyCode": "EUR",
+        },
+        "tiers": [],
+      }
+    `);
   });
 
   it('should return a eur50000 preset when built for graphql', () => {
     const eur50000PresetGraphql =
       eur50000().buildGraphql<TShippingRateDraftGraphql>();
 
-    expect(eur50000PresetGraphql).toMatchObject({
-      price: { centAmount: 50000, currencyCode: 'EUR' },
-      tiers: [],
-    });
+    expect(eur50000PresetGraphql).toMatchInlineSnapshot(`
+      {
+        "freeAbove": undefined,
+        "price": {
+          "centAmount": 50000,
+          "currencyCode": "EUR",
+        },
+        "tiers": [],
+      }
+    `);
   });
 });
--- models/shipping-method/src/shipping-rate/shipping-rate-draft/presets/sample-data-goodstore/gbp-50000.spec.ts
+++ models/shipping-method/src/shipping-rate/shipping-rate-draft/presets/sample-data-goodstore/gbp-50000.spec.ts
@@ -8,19 +8,31 @@ describe('with gbp50000 preset', () => {
   it('should return a gbp50000 preset', () => {
     const gbp50000Preset = gbp50000().build<TShippingRateDraft>();
 
-    expect(gbp50000Preset).toMatchObject({
-      price: { centAmount: 50000, currencyCode: 'GBP' },
-      tiers: [],
-    });
+    expect(gbp50000Preset).toMatchInlineSnapshot(`
+      {
+        "freeAbove": undefined,
+        "price": {
+          "centAmount": 50000,
+          "currencyCode": "GBP",
+        },
+        "tiers": [],
+      }
+    `);
   });
 
   it('should return a gbp50000 preset when built for graphql', () => {
     const gbp50000PresetGraphql =
       gbp50000().buildGraphql<TShippingRateDraftGraphql>();
 
-    expect(gbp50000PresetGraphql).toMatchObject({
-      price: { centAmount: 50000, currencyCode: 'GBP' },
-      tiers: [],
-    });
+    expect(gbp50000PresetGraphql).toMatchInlineSnapshot(`
+      {
+        "freeAbove": undefined,
+        "price": {
+          "centAmount": 50000,
+          "currencyCode": "GBP",
+        },
+        "tiers": [],
+      }
+    `);
   });
 });
