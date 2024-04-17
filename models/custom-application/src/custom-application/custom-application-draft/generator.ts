0fa991dcfa46725c989b129e292800fba653ccd5
Yemitan Isaiah Olurotimi <isaiah.yemitan@commercetools.com>
2024-04-17 16:53:08 +0200

    refactor: use slugify for entryPointUriPath

--- models/custom-application/src/custom-application/custom-application-draft/generator.ts
+++ models/custom-application/src/custom-application/custom-application-draft/generator.ts
@@ -1,4 +1,5 @@
 import { fake, Generator } from '@commercetools-test-data/core';
+import { slugify } from '@commercetools-test-data/utils';
 import { CustomApplicationMenuLinkDraft } from '../../custom-application-menu-link';
 import { CustomApplicationPermissionDraft } from '../../custom-application-permission';
 import { CustomApplicationSubmenuLinkDraft } from '../../custom-application-submenu-link';
@@ -14,7 +15,7 @@ const generator = Generator<TCustomApplicationDraft>({
     ]),
     icon: '<svg><path fill="#000000" /></svg>',
     name: fake((f) => f.commerce.department()),
-    entryPointUriPath: fake((f) => f.lorem.word()),
+    entryPointUriPath: fake((f) => slugify(f.lorem.word())),
     mainMenuLink: fake(() => CustomApplicationMenuLinkDraft.random()),
     submenuLinks: [fake(() => CustomApplicationSubmenuLinkDraft.random())],
   },
