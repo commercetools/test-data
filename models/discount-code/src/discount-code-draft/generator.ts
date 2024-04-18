e785197b5a99e5fac873ee521126fa5fe3b99d1c
Kevin Terry <kevin.terry@commercetools.com>
2024-04-18 09:55:35 -0400

    fix(discount-code-draft): updated code property

--- models/discount-code/src/discount-code-draft/generator.ts
+++ models/discount-code/src/discount-code-draft/generator.ts
@@ -12,7 +12,7 @@ const generator = Generator<TDiscountCodeDraft>({
   fields: {
     name: fake(() => LocalizedStringDraft.random()),
     description: fake(() => LocalizedStringDraft.random()),
-    code: fake((f) => f.lorem.word()),
+    code: fake((f) => f.lorem.words({ min: 1, max: 3 })),
     cartDiscounts: [],
     cartPredicate: '1=1',
     isActive: fake((f) => f.datatype.boolean()),
