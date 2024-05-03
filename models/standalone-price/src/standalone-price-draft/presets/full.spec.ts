ac6eeff663d8012ed65fddb9d38a7783fd768cca
Nima Jahanshahlou <nima.jahanshahlou@commercetools.com>
2024-05-03 15:06:43 +0200

    feat(): update sp draft preset and tests

--- models/standalone-price/src/standalone-price-draft/presets/full.spec.ts
+++ models/standalone-price/src/standalone-price-draft/presets/full.spec.ts
@@ -91,4 +91,98 @@ describe(`with full preset`, () => {
       active: expect.any(Boolean),
     });
   });
+
+  it(`should return a full preset with a unique currency code when an argument is provided when built for rest`, () => {
+    const standalonePriceDraft = full({
+      currency: 'EUR',
+    }).buildRest<TStandalonePriceDraft>();
+
+    expect(standalonePriceDraft).toEqual(
+      expect.objectContaining({
+        key: expect.any(String),
+        sku: expect.any(String),
+        value: expect.objectContaining({
+          centAmount: expect.any(Number),
+          currencyCode: 'EUR',
+        }),
+        country: expect.any(String),
+        customerGroup: expect.objectContaining({
+          typeId: 'customer-group',
+          key: expect.any(String),
+        }),
+        channel: expect.objectContaining({
+          typeId: 'channel',
+          key: expect.any(String),
+        }),
+        validFrom: expect.any(String),
+        validUntil: expect.any(String),
+        tiers: expect.arrayContaining([
+          expect.objectContaining({
+            minimumQuantity: expect.any(Number),
+            value: expect.objectContaining({
+              centAmount: expect.any(Number),
+              currencyCode: 'EUR',
+              type: expect.any(String),
+              fractionDigits: expect.any(Number),
+            }),
+          }),
+        ]),
+        discounted: null,
+        staged: expect.objectContaining({
+          value: expect.objectContaining({
+            currencyCode: 'EUR',
+          }),
+        }),
+        active: expect.any(Boolean),
+      })
+    );
+  });
+
+  it(`should return a full preset with a unique currency code when an argument is provided when built for graphql`, () => {
+    const standalonePriceDraft = full({
+      currency: 'EUR',
+    }).buildGraphql<TStandalonePriceDraft>();
+
+    expect(standalonePriceDraft).toEqual(
+      expect.objectContaining({
+        key: expect.any(String),
+        sku: expect.any(String),
+        value: expect.objectContaining({
+          centPrecision: expect.objectContaining({
+            centAmount: expect.any(Number),
+            currencyCode: 'EUR',
+          }),
+        }),
+        country: expect.any(String),
+        customerGroup: expect.objectContaining({
+          typeId: 'customer-group',
+          key: expect.any(String),
+        }),
+        channel: expect.objectContaining({
+          typeId: 'channel',
+          key: expect.any(String),
+        }),
+        validFrom: expect.any(String),
+        validUntil: expect.any(String),
+        tiers: expect.arrayContaining([
+          expect.objectContaining({
+            minimumQuantity: expect.any(Number),
+            value: expect.objectContaining({
+              centAmount: expect.any(Number),
+              currencyCode: 'EUR',
+              type: expect.any(String),
+              fractionDigits: expect.any(Number),
+            }),
+          }),
+        ]),
+        discounted: null,
+        staged: expect.objectContaining({
+          value: expect.objectContaining({
+            currencyCode: 'EUR',
+          }),
+        }),
+        active: expect.any(Boolean),
+      })
+    );
+  });
 });
--- models/standalone-price/src/standalone-price-draft/presets/full.ts
+++ models/standalone-price/src/standalone-price-draft/presets/full.ts
@@ -12,13 +12,25 @@ import StandalonePriceDraft from '../builder';
  * Note:
  * Due to a implementation restriction on the transformer, the default build will not include a unified currency.
  *
+ * @param {Object} [options] - Optional parameters to customize the price draft.
+ * @param {string} [options.currency] - Set manually the provided currency code for all money values if provided.
  * @returns {TStandalonePriceDraftBuilder} A standalone price draft builder.
  *
  * @example
  * const standalonePriceDraft = StandalonePriceDraft.presets.full().build();
+ * const standalonePriceDraft = StandalonePriceDraft.presets.full({currency: 'EUR'}).build();
  */
-const full = (): TStandalonePriceDraftBuilder => {
+const full = (options?: {
+  currency?: string;
+}): TStandalonePriceDraftBuilder => {
+  const draftValue = MoneyDraft.random();
+
+  if (options?.currency) {
+    draftValue.currencyCode(options.currency);
+  }
+
   return StandalonePriceDraft()
+    .value(draftValue)
     .tiers([
       PriceTierDraft.random(),
       PriceTierDraft.random(),
