5dd0a4a4f0aaf02ec0f40ca288799e4bddf1ae14
Nima Jahanshahlou <nima.jahanshahlou@commercetools.com>
2024-05-06 16:46:11 +0200

    feat(): add export for StagedPriceDraft

--- models/standalone-price/src/index.ts
+++ models/standalone-price/src/index.ts
@@ -1,4 +1,5 @@
 export * as StandalonePriceDraft from './standalone-price-draft';
+export * as StagedPriceDraft from './staged-price-draft';
 export * as StandalonePrice from '.';
 
 export { default as random } from './builder';
