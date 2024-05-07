88cdc982bee41a6d65311ffceb014d7160acf375
Nima Jahanshahlou <nima.jahanshahlou@commercetools.com>
2024-05-07 09:14:30 +0200

    chore(): remove staged-price type references

--- models/standalone-price/src/staged-price-draft/types.ts
+++ models/standalone-price/src/staged-price-draft/types.ts
@@ -1,18 +1,9 @@
-import {
-  StagedStandalonePrice,
-  StagedPriceDraft,
-} from '@commercetools/platform-sdk';
+import { StagedPriceDraft } from '@commercetools/platform-sdk';
 import { TBuilder } from '@commercetools-test-data/core';
 
-export type TStagedPrice = StagedStandalonePrice;
 export type TStagedPriceDraft = StagedPriceDraft;
 
-export type TStagedPriceGraphql = TStagedPrice & {
-  __typename: 'StagedStandalonePrice';
-};
 export type TStagedPriceDraftGraphql = TStagedPriceDraft;
 
-export type TStagedPriceBuilder = TBuilder<TStagedPrice>;
 export type TStagedPriceDraftBuilder = TBuilder<TStagedPriceDraft>;
-export type TCreateStagedPriceBuilder = () => TStagedPriceBuilder;
 export type TCreateStagedPriceDraftBuilder = () => TStagedPriceDraftBuilder;
