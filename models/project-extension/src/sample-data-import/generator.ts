3552ce66b0252482870ab9e89c8560fbe9704404
Yemitan Isaiah Olurotimi <isaiah.yemitan@commercetools.com>
2024-05-14 12:39:42 +0200

    refactor: update sample data import dataset

--- models/project-extension/src/sample-data-import/generator.ts
+++ models/project-extension/src/sample-data-import/generator.ts
@@ -5,7 +5,9 @@ import type { TSampleDataImportMetadata } from './types';
 const generator = Generator<TSampleDataImportMetadata>({
   fields: {
     completed: fake((f) => f.datatype.boolean()),
-    dataset: fake((f) => f.helpers.arrayElement(['FASHION', 'GOODSTORE'])),
+    dataset: fake((f) =>
+      f.helpers.arrayElement(['FASHION', 'GOODSTORE', 'B2B'])
+    ),
   },
 });
 
