9b06d92d283ca597dcdfa89b14911ca259f850c6
Nima Jahanshahlou <nima.jahanshahlou@commercetools.com>
2024-08-15 16:46:36 +0200

    feat(): add StagedStandalonePrice model

---
+++ models/standalone-price/src/staged-standalone-price/generator.ts
@@ -0,0 +1,14 @@
+import { Money } from '@commercetools-test-data/commons';
+import { fake, Generator } from '@commercetools-test-data/core';
+import { TStagedStandalonePrice } from './types';
+
+// https://docs.commercetools.com/api/projects/standalone-prices#stagedstandaloneprice
+
+const generator = Generator<TStagedStandalonePrice>({
+  fields: {
+    value: fake(() => Money.random()),
+    discounted: null,
+  },
+});
+
+export default generator;
