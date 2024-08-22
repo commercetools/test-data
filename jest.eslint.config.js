aca5a92b33c91c8d621c15cf6832830cd9afe86a
Nima Jahanshahlou <nima.jahanshahlou@commercetools.com>
2024-08-22 16:25:53 +0200

    fix(): update jest.eslint.config.js

--- jest.eslint.config.js
+++ jest.eslint.config.js
@@ -5,7 +5,7 @@ module.exports = {
   runner: 'jest-runner-eslint',
   displayName: 'eslint',
   moduleFileExtensions: ['js', 'ts', 'tsx'],
-  modulePathIgnorePatterns: ['build', 'dist', 'public', 'generated'],
+  modulePathIgnorePatterns: ['/build/', 'dist', 'public', 'generated'],
   testMatch: ['<rootDir>/**/*.js', '<rootDir>/**/*.ts', '<rootDir>/**/*.tsx'],
   watchPlugins: ['jest-watch-typeahead/filename'],
 };
