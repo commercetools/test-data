9fd53a3bd7dcd31e9ce5f1000e884221eac02c89
Jaikumar.T.J <jaikumar.jayapal@commercetools.com>
2024-08-26 14:33:22 +0200

    fix: make supply channel optional in inventory-entry

--- models/inventory-entry/src/builder.spec.ts
+++ models/inventory-entry/src/builder.spec.ts
@@ -96,3 +96,53 @@ describe('builder', () => {
     )
   );
 });
+
+describe('builder without supply channel', () => {
+  it(
+    ...createBuilderSpec<TInventoryEntry, TInventoryEntryRest>(
+      'rest',
+      InventoryEntry.random().supplyChannel(null),
+      expect.objectContaining({
+        id: expect.any(String),
+        key: expect.any(String),
+        version: expect.any(Number),
+        createdAt: expect.any(String),
+        createdBy: null,
+        lastModifiedAt: expect.any(String),
+        lastModifiedBy: null,
+        sku: expect.any(String),
+        quantityOnStock: expect.any(Number),
+        restockableInDays: expect.any(Number),
+        availableQuantity: expect.any(Number),
+        expectedDelivery: expect.any(String),
+        supplyChannel: undefined,
+        custom: null,
+      })
+    )
+  );
+
+  it(
+    ...createBuilderSpec<TInventoryEntry, TInventoryEntryGraphql>(
+      'graphql',
+      InventoryEntry.random().supplyChannel(null),
+      expect.objectContaining({
+        id: expect.any(String),
+        key: expect.any(String),
+        version: expect.any(Number),
+        createdAt: expect.any(String),
+        createdBy: null,
+        lastModifiedAt: expect.any(String),
+        lastModifiedBy: null,
+        sku: expect.any(String),
+        quantityOnStock: expect.any(Number),
+        restockableInDays: expect.any(Number),
+        availableQuantity: expect.any(Number),
+        expectedDelivery: expect.any(String),
+        supplyChannel: null,
+        supplyChannelRef: undefined,
+        custom: null,
+        __typename: 'InventoryEntry',
+      })
+    )
+  );
+});
