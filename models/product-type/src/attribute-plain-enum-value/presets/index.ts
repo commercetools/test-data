41b0a55cb7fe1dacc0b428be4469965921d38ca2
Jaikumar.T.J <jaikumar.jayapal@commercetools.com>
2024-08-27 06:58:19 +0200

    chore: add enum lenum presets and total in transformer

--- models/product-type/src/attribute-plain-enum-value/presets/index.ts
+++ models/product-type/src/attribute-plain-enum-value/presets/index.ts
@@ -1,3 +1,13 @@
-const presets = {};
+import AttributePlainEnumValue from '../builder';
+
+const presets = {
+  red: () => AttributePlainEnumValue().key('red').label('Red color'),
+  blue: () => AttributePlainEnumValue().key('blue').label('Blue color'),
+  green: () => AttributePlainEnumValue().key('green').label('Green color'),
+  s: () => AttributePlainEnumValue().key('s').label('Size S'),
+  m: () => AttributePlainEnumValue().key('m').label('Size M'),
+  l: () => AttributePlainEnumValue().key('l').label('Size L'),
+  xl: () => AttributePlainEnumValue().key('xl').label('Size XL'),
+};
 
 export default presets;
