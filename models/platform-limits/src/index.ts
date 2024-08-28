e3d66a33d2ea13b25870e9c08104be9a8f7be932
Jaikumar.T.J <jaikumar.jayapal@commercetools.com>
2024-08-28 09:56:11 +0200

    chore: update index file

--- models/platform-limits/src/index.ts
+++ models/platform-limits/src/index.ts
@@ -4,7 +4,14 @@ export * from './cart-discounts/types';
 export * from './carts/types';
 export * from './customers/types';
 export * from './customer-groups/types';
+export * from './general/types';
 export * from './limit-with-current/types';
+export * from './product-discounts/types';
+export * from './shipping-methods/types';
+export * from './shopping-lists/types';
+export * from './stores/types';
+export * from './tax-categories/types';
+export * from './zones/types';
 
 // Export models
 export * as BusinessUnitLimitsProjection from './business-units';
@@ -12,4 +19,11 @@ export * as CartDiscountLimitsProjection from './cart-discounts';
 export * as CartLimitsProjection from './carts';
 export * as CustomerLimitsProjection from './customers';
 export * as CustomerGroupLimitsProjection from './customer-groups';
+export * as PlatformLimits from './general';
 export * as LimitWithCurrent from './limit-with-current';
+export * as ProductDiscountLimitsProjection from './product-discounts';
+export * as ShippingMethodLimitsProjection from './shipping-methods';
+export * as ShoppingListLimitsProjection from './shopping-lists';
+export * as StoreLimitsProjection from './stores';
+export * as TaxCategoryLimitsProjection from './tax-categories';
+export * as ZoneLimitsProjection from './zones';
