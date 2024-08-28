4577b87c1ac53359bca15a78d8a5c01b10506aae
Jaikumar.T.J <jaikumar.jayapal@commercetools.com>
2024-08-28 08:33:34 +0200

    chore: rename export to match API model

--- models/platform-limits/src/index.ts
+++ models/platform-limits/src/index.ts
@@ -1,9 +1,7 @@
 // Export types
 export * from './business-units/types';
-export * from './limit/types';
 export * from './limit-with-current/types';
 
 // Export models
-export * as BusinessUnits from './business-units';
-export * as Limit from './limit';
+export * as BusinessUnitLimitsProjection from './business-units';
 export * as LimitWithCurrent from './limit-with-current';
