b325d9c4d30e1eed6514b34390c89c91c552c6cc
Jaikumar.T.J <jaikumar.jayapal@commercetools.com>
2024-08-28 08:39:52 +0200

    chore: update index file

--- models/platform-limits/src/index.ts
+++ models/platform-limits/src/index.ts
@@ -1,7 +1,15 @@
 // Export types
 export * from './business-units/types';
+export * from './cart-discounts/types';
+export * from './carts/types';
+export * from './customers/types';
+export * from './customer-groups/types';
 export * from './limit-with-current/types';
 
 // Export models
 export * as BusinessUnitLimitsProjection from './business-units';
+export * as CartDiscountLimitsProjection from './cart-discounts';
+export * as CartLimitsProjection from './carts';
+export * as CustomerLimitsProjection from './customers';
+export * as CustomerGroupLimitsProjection from './customer-groups';
 export * as LimitWithCurrent from './limit-with-current';
