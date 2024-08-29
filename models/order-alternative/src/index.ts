42b6f323041626dc4ee9adeb748638980ecc532a
Carlos Cortizas <carlos.martines@commercetools.com>
2024-08-29 11:41:52 +0200

    fix: adjust presets imports and exports

--- models/order-alternative/src/index.ts
+++ models/order-alternative/src/index.ts
@@ -1,19 +1,23 @@
 import { RestModelBuilder, GraphqlModelBuilder } from './builders';
+import * as draftPresets from './order-from-cart-draft/presets';
+import * as presets from './presets';
 
 export * as OrderFromCartDraft from './order-from-cart-draft';
 export * as OrderFromQuoteDraft from './order-from-quote-draft';
 export * as Order from '.';
 
 export const random = RestModelBuilder;
-export { default as presets } from './presets';
-export { default as draftPresets } from './order-from-cart-draft/presets';
 
 export * from './types';
 export * as constants from './constants';
 
 export const OrderRest = {
   random: RestModelBuilder,
+  presets: presets.restPresets,
+  draftPresets: draftPresets.restPresets,
 };
 export const OrderGraphql = {
   random: GraphqlModelBuilder,
+  presets: presets.graphqlPresets,
+  draftPresets: draftPresets.graphqlPresets,
 };
--- models/order-alternative/src/order-from-cart-draft/index.ts
+++ models/order-alternative/src/order-from-cart-draft/index.ts
@@ -1,12 +1,14 @@
 import { RestModelBuilder, GraphqlModelBuilder } from './builders';
+import { restPresets, graphqlPresets } from './presets';
 
 // export { default as random } from './builder';
 export const random = RestModelBuilder;
-export { default as presets } from './presets';
 
 export const OrderFromCartDraftRest = {
   random: RestModelBuilder,
+  presets: restPresets,
 };
 export const OrderFromCartDraftGraphql = {
   random: GraphqlModelBuilder,
+  presets: graphqlPresets,
 };
