617107cad9f4c095a0f686bf60262289367ca677
Jaikumar.T.J <jaikumar.jayapal@commercetools.com>
2024-08-29 12:45:06 +0200

    chore: use constant

--- models/platform-limits/src/business-units/transformers.ts
+++ models/platform-limits/src/business-units/transformers.ts
@@ -3,40 +3,30 @@ import {
   TBusinessUnitLimitsProjection,
   TBusinessUnitLimitsProjectionGraphql,
 } from './types';
-
+const buildFields: (keyof TBusinessUnitLimitsProjection)[] = [
+  'maxDivisions',
+  'maxDepthLimit',
+  'maxAssociates',
+  'maxAssociateRoles',
+];
 const transformers = {
   default: Transformer<
     TBusinessUnitLimitsProjection,
     TBusinessUnitLimitsProjection
   >('default', {
-    buildFields: [
-      'maxDivisions',
-      'maxDepthLimit',
-      'maxAssociates',
-      'maxAssociateRoles',
-    ],
+    buildFields,
   }),
   rest: Transformer<
     TBusinessUnitLimitsProjection,
     TBusinessUnitLimitsProjection
   >('rest', {
-    buildFields: [
-      'maxDivisions',
-      'maxDepthLimit',
-      'maxAssociates',
-      'maxAssociateRoles',
-    ],
+    buildFields,
   }),
   graphql: Transformer<
     TBusinessUnitLimitsProjection,
     TBusinessUnitLimitsProjectionGraphql
   >('graphql', {
-    buildFields: [
-      'maxDivisions',
-      'maxDepthLimit',
-      'maxAssociates',
-      'maxAssociateRoles',
-    ],
+    buildFields,
     addFields: () => ({
       __typename: 'BusinessUnitLimitsProjection',
     }),
