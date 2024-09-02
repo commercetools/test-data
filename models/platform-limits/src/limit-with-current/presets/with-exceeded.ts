fcd86866594399f9f3a762d5eaa7b027d81aa686
Jaikumar.T.J <jaikumar.jayapal@commercetools.com>
2024-09-02 06:35:19 +0200

    chore: add comments

--- models/platform-limits/src/limit-with-current/presets/with-exceeded.ts
+++ models/platform-limits/src/limit-with-current/presets/with-exceeded.ts
@@ -1,5 +1,6 @@
 import LimitWithCurrent from '../builder';
 
+// The current value as >= 100% of the limit is considered as exceeded.
 const withExceeded = () => LimitWithCurrent().limit(10).current(10);
 
 export default withExceeded;
