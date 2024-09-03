e1034c82cb7c8c98024a24caee0b6dbf060e9e7e
antoniolodias <antonio.dias@commercetools.com>
2024-09-03 15:11:55 +0100

    chore: add shipping model

--- models/cart/src/index.ts
+++ models/cart/src/index.ts
@@ -10,3 +10,4 @@ export * as CustomLineItem from './custom-line-item';
 export * as CustomLineItemDraft from './custom-line-item/custom-line-item-draft';
 export * as LineItem from './line-item';
 export * as LineItemDraft from './line-item/line-item-draft';
+export * as ShippingInfo from './shipping-info';
