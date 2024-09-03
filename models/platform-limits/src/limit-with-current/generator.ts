aa7ad4e28b70b1fa5e0b1d52d4c63aaa99db10d1
Jaikumar.T.J <jaikumar.jayapal@commercetools.com>
2024-09-03 05:42:53 +0200

    chore: change default current maximum value

--- models/platform-limits/src/limit-with-current/generator.ts
+++ models/platform-limits/src/limit-with-current/generator.ts
@@ -1,14 +1,10 @@
 import { fake, Generator } from '@commercetools-test-data/core';
 import { TLimitWithCurrent } from './types';
 
-/**
- * limit - Number
- * current - Number
- */
 const generator = Generator<TLimitWithCurrent>({
   fields: {
     limit: fake((f) => f.number.int(10)),
-    current: fake((f) => f.number.int(10)),
+    current: fake((f) => f.number.int(7)),
   },
 });
 
