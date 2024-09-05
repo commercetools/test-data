145fd9bd1b120fefc79d575e80184bae01eb9209
Carlos Cortizas <carlos.martines@commercetools.com>
2024-09-05 16:08:39 +0200

    refactor(delivery): update tests

--- models/order/src/delivery/builder.spec.ts
+++ models/order/src/delivery/builder.spec.ts
@@ -2,81 +2,63 @@
 /* eslint-disable jest/valid-title */
 
 import { Address } from '@commercetools-test-data/commons';
-import { createBuilderSpec } from '@commercetools-test-data/core/test-utils';
-import { DeliveryItem } from '../delivery-item';
-import { Parcel } from '../parcel';
 import { TDelivery, TDeliveryGraphql } from './types';
 import * as Delivery from '.';
 
-describe('builder', () => {
-  const defaultDeliverySpec = {
-    id: expect.any(String),
-    key: null,
-    items: expect.arrayContaining([
-      expect.objectContaining({
-        id: expect.any(String),
-        quantity: expect.any(Number),
-      }),
-    ]),
-    parcels: expect.arrayContaining([
-      expect.objectContaining({
-        id: expect.any(String),
-        createdAt: expect.any(String),
-      }),
-    ]),
-    address: null,
-    custom: null,
-    createdAt: expect.any(String),
-  };
+describe('Delviery builder', () => {
+  const deliveryMockBuilder = Delivery.random()
+    .key('delivery-key-1')
+    .address(Address.random());
 
-  it(
-    ...createBuilderSpec<TDelivery, TDelivery>(
-      'default',
-      Delivery.random(),
-      expect.objectContaining(defaultDeliverySpec)
-    )
-  );
-
-  it(
-    ...createBuilderSpec<TDelivery, TDeliveryGraphql>(
-      'graphql',
-      Delivery.random(),
-      expect.objectContaining({
-        ...defaultDeliverySpec,
-        __typename: 'Delivery',
-      })
-    )
-  );
-
-  it('should allow customization', () => {
-    const id = 'test-id';
-    const key = 'test key';
-    const streetName = 'test street name';
-    const createdAt = '2024-08-27T19:26:08.429Z';
-
-    const item = DeliveryItem.random().id(id);
-    const parcel = Parcel.random().id(id);
-    const address = Address.random().streetName(streetName);
-
-    const deliveryMock = Delivery.random()
-      .id(id)
-      .key(key)
-      .items([item])
-      .parcels([parcel])
-      .address(address)
-      // .custom()
-      .createdAt(createdAt)
-      .buildGraphql();
+  it('should build rest representation', () => {
+    const deliveryMock = deliveryMockBuilder.buildRest<TDelivery>();
 
     expect(deliveryMock).toEqual(
       expect.objectContaining({
-        id,
-        key,
-        items: expect.arrayContaining([expect.objectContaining({ id })]),
-        parcels: expect.arrayContaining([expect.objectContaining({ id })]),
-        address: expect.objectContaining({ streetName }),
-        // custom:
-        createdAt,
+        id: expect.any(String),
+        key: 'delivery-key-1',
+        items: expect.arrayContaining([
+          expect.objectContaining({
+            quantity: expect.any(Number),
+          }),
+        ]),
+        parcels: expect.arrayContaining([
+          expect.objectContaining({
+            id: expect.any(String),
+          }),
+        ]),
+        address: expect.objectContaining({
+          firstName: expect.any(String),
+        }),
+        createdAt: expect.any(String),
+      })
+    );
+  });
+
+  it('should build grapqhl representation', () => {
+    const deliveryMock = deliveryMockBuilder.buildGraphql<TDeliveryGraphql>();
+
+    expect(deliveryMock).toEqual(
+      expect.objectContaining({
+        id: expect.any(String),
+        key: 'delivery-key-1',
+        items: expect.arrayContaining([
+          expect.objectContaining({
+            quantity: expect.any(Number),
+            __typename: 'DeliveryItem',
+          }),
+        ]),
+        parcels: expect.arrayContaining([
+          expect.objectContaining({
+            id: expect.any(String),
+            __typename: 'Parcel',
+          }),
+        ]),
+        address: expect.objectContaining({
+          firstName: expect.any(String),
+          __typename: 'Address',
+        }),
+        createdAt: expect.any(String),
         __typename: 'Delivery',
       })
     );
