35bd4f6589f22f49e751dbba5ec8a546055c0d66
Carlos Cortizas <carlos.martines@commercetools.com>
2024-10-23 17:27:53 +0200

    refactor: group quotes related models

---
+++ models/quote/src/quote/types.ts
@@ -0,0 +1,57 @@
+import type {
+  BusinessUnit,
+  Customer,
+  CustomerGroup,
+  Quote,
+  QuoteDraft,
+  StagedQuote,
+  State,
+  Store,
+} from '@commercetools/platform-sdk';
+import {
+  TReferenceGraphql,
+  TKeyReferenceGraphql,
+} from '@commercetools-test-data/commons';
+import type { TBuilder } from '@commercetools-test-data/core';
+import { TQuoteRequestGraphql } from '../quote-request';
+
+// Default
+export type TQuote = Omit<
+  Quote,
+  | 'businessUnit'
+  | 'customer'
+  | 'customerGroup'
+  | 'quoteRequest'
+  | 'stagedQuote'
+  | 'state'
+  | 'store'
+> & {
+  businessUnit: BusinessUnit;
+  customer: Customer;
+  customerGroup: CustomerGroup;
+  quoteRequest: TQuoteRequestGraphql;
+  stagedQuote: StagedQuote;
+  state: State;
+  store: Store;
+};
+
+// Rest
+export type TQuoteRest = Quote;
+export type TQuoteDraft = QuoteDraft;
+
+export type TQuoteGraphql = TQuote & {
+  businessUnitRef: TKeyReferenceGraphql | null;
+  customerRef: TReferenceGraphql | null;
+  customerGroupRef: TReferenceGraphql | null;
+  quoteReqestRef: TReferenceGraphql;
+  stagedQuoteRef: TReferenceGraphql;
+  stateRef: TReferenceGraphql | null;
+  storeRef: TKeyReferenceGraphql | null;
+  __typename: 'Quote';
+};
+export type TQuoteDraftGraphql = TQuoteDraft;
+
+export type TQuoteBuilder = TBuilder<TQuote>;
+export type TQuoteDraftBuilder = TBuilder<TQuoteDraft>;
+export type TCreateQuoteBuilder = () => TQuoteBuilder;
+export type TCreateQuoteDraftBuilder = () => TQuoteDraftBuilder;
