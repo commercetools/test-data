6b41192d5fbe55777874b74637ac874229daa651
Mario Arriola <mario.arriola@commercetools.com>
2024-10-24 14:38:50 +0200

    chore: implement types

--- models/discounted-price/src/types.ts
+++ models/discounted-price/src/types.ts
@@ -1,23 +1,21 @@
 import { DiscountedPrice } from '@commercetools/platform-sdk';
+import { TReference } from '@commercetools-test-data/commons';
 import type { TBuilder } from '@commercetools-test-data/core';
 
 export type TDiscountedPriceRest = DiscountedPrice;
 
-// TODO: Check wether the graphql version has different properties
-// or types than the rest version.
-// You might want to use the GraphQL Explorer for that
-// (https://<merchant_center_domain>/<project_key>/graphql-explorer-commercetools/platform)
 export type TDiscountedPriceGraphql = DiscountedPrice & {
-  __typename: 'DiscountedPrice';
+  __typename: 'DiscountedProductPriceValue';
+  discountRef: TReference;
 };
 
-// TODO: Implement draft models if needed
-// Otherwise remove this code
-// export type TDiscountedPriceDraftRest = OrderDraft;
-// export type TDiscountedPriceDraftGraphql = OrderDraft;
+export type TDiscountedPriceDraftRest = TDiscountedPriceRest;
+export type TDiscountedPriceDraftGraphql = TDiscountedPriceRest;
 
 export type TCreateDiscountedPriceBuilder<
-  TModel extends TDiscountedPriceRest | TDiscountedPriceGraphql,
-  // | TDiscountedPriceDraftRest
-  // | TDiscountedPriceDraftGraphql
+  TModel extends
+    | TDiscountedPriceRest
+    | TDiscountedPriceGraphql
+    | TDiscountedPriceDraftRest
+    | TDiscountedPriceDraftGraphql,
 > = () => TBuilder<TModel>;
