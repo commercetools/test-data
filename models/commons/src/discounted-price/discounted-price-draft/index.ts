3eec618f50b5e798b19c2da0ad3ebd81c30c05c6
Mario Arriola <mario.arriola@commercetools.com>
2024-10-31 06:47:48 +0100

    fix: add presets back

--- models/commons/src/discounted-price/discounted-price-draft/index.ts
+++ models/commons/src/discounted-price/discounted-price-draft/index.ts
@@ -1,9 +1,12 @@
 import { RestModelBuilder, GraphqlModelBuilder } from './builders';
+import * as DiscountedPriceDraftPresets from './presets';
 
 export const DiscountedPriceDraftRest = {
   random: RestModelBuilder,
+  presets: DiscountedPriceDraftPresets.restPresets,
 };
 
 export const DiscountedPriceDraftGraphql = {
   random: GraphqlModelBuilder,
+  presets: DiscountedPriceDraftPresets.graphqlPresets,
 };
