47cb2dc2f08b85a7e2a35912108b32a0ce64502b
Jonathan Creasman <jonathan.creasman@commercetools.com>
2024-11-05 14:17:45 -0500

    feat(inventory entry): remove unused imports

--- models/inventory-entry/src/inventory-entry-draft/builder.spec.ts
+++ models/inventory-entry/src/inventory-entry-draft/builder.spec.ts
@@ -1,11 +1,5 @@
 /* eslint-disable jest/no-disabled-tests */
 /* eslint-disable jest/valid-title */
-import { createBuilderSpec } from '@commercetools-test-data/core/test-utils';
-import {
-  TInventoryEntryDraft,
-  TInventoryEntryDraftRest,
-  TInventoryEntryDraftGraphql,
-} from '../types';
 import {
   InventoryEntryDraft,
   InventoryEntryDraftRest,
