0379b83dcecaead555829747a4af362bf8e665d6
Rafa Gares <rafael.gares@commercetools.com>
2025-02-11 18:28:02 +0100

    fix(product): wrong `TProductGraphql` and `TProductCatalogDataGraphql` types

--- models/product/src/product-catalog-data/types.ts
+++ models/product/src/product-catalog-data/types.ts
@@ -1,11 +1,10 @@
 import { ProductCatalogData } from '@commercetools/platform-sdk';
 import type { TBuilder } from '@commercetools-test-data/core';
+import type { TCtpProductCatalogData } from '@commercetools-test-data/graphql-types';
 
 export type TProductCatalogData = ProductCatalogData;
 
-export type TProductCatalogDataGraphql = TProductCatalogData & {
-  __typename: 'ProductCatalogData';
-};
+export type TProductCatalogDataGraphql = TCtpProductCatalogData;
 
 export type TProductCatalogDataBuilder = TBuilder<TProductCatalogData>;
 export type TCreateProductCatalogDataBuilder = () => TProductCatalogDataBuilder;
