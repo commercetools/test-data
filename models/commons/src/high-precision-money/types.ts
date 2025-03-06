9b1de9fd3682209fbfa47f96c1fa8f74fb3a85f3
Rafa Gares <rafael.gares@commercetools.com>
2025-03-06 09:44:05 +0100

    fix: improve Money and HighPrecisionMoney GraphQL models

--- models/commons/src/high-precision-money/types.ts
+++ models/commons/src/high-precision-money/types.ts
@@ -3,14 +3,16 @@ import {
   HighPrecisionMoneyDraft,
 } from '@commercetools/platform-sdk';
 import type { TBuilder } from '@commercetools-test-data/core';
+import {
+  TCtpHighPrecisionMoney,
+  TCtpHighPrecisionMoneyInput,
+} from '@commercetools-test-data/graphql-types';
 
 export type THighPrecisionMoney = HighPrecisionMoney;
 export type THighPrecisionMoneyDraft = HighPrecisionMoneyDraft;
 
-export type THighPrecisionMoneyGraphql = THighPrecisionMoney & {
-  __typename: 'HighPrecisionMoney';
-};
-export type THighPrecisionMoneyDraftGraphql = THighPrecisionMoneyDraft;
+export type THighPrecisionMoneyGraphql = TCtpHighPrecisionMoney;
+export type THighPrecisionMoneyDraftGraphql = TCtpHighPrecisionMoneyInput;
 
 export type THighPrecisionMoneyBuilder = TBuilder<THighPrecisionMoney>;
 export type THighPrecisionMoneyDraftBuilder =
