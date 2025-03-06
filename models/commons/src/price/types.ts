3d71328d245aadabd4a4e887ee11512bab4fda16
Rafa Gares <rafael.gares@commercetools.com>
2025-03-06 13:03:14 +0100

    fix: PriceGraphql transformers

--- models/commons/src/price/types.ts
+++ models/commons/src/price/types.ts
@@ -1,18 +1,15 @@
 import type { Price, PriceDraft } from '@commercetools/platform-sdk';
 import type { TBuilder } from '@commercetools-test-data/core';
-import { TMoneyGraphql } from '../money';
+import {
+  TCtpProductPrice,
+  TCtpProductPriceDataInput,
+} from '@commercetools-test-data/graphql-types';
 
 export type TPrice = Price;
 export type TPriceDraft = PriceDraft;
 
-export type TPriceGraphql = TPrice & {
-  __typename: 'ProductPrice';
-};
-export type TPriceDraftGraphql = Omit<TPriceDraft, 'value'> & {
-  value: {
-    centPrecision: TMoneyGraphql;
-  };
-};
+export type TPriceGraphql = TCtpProductPrice;
+export type TPriceDraftGraphql = TCtpProductPriceDataInput;
 
 export type TPriceBuilder = TBuilder<Price>;
 export type TPriceDraftBuilder = TBuilder<PriceDraft>;
