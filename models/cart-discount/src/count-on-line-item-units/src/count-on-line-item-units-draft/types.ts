5c0c1eef3e0701fc82002c13248de5537372c35c
Mario Arriola <mario.arriola@commercetools.com>
2025-03-26 08:47:04 +0100

    refactor(cart-discount): omit excludeCount from TCountOnLineItemUnitsDraftGraphql type

--- models/cart-discount/src/count-on-line-item-units/src/count-on-line-item-units-draft/types.ts
+++ models/cart-discount/src/count-on-line-item-units/src/count-on-line-item-units-draft/types.ts
@@ -1,7 +1,10 @@
 import type { TBuilder } from '@commercetools-test-data/core';
 import { TCtpCountOnLineItemUnitsInput } from '@commercetools-test-data/graphql-types';
 
-export type TCountOnLineItemUnitsDraftGraphql = TCtpCountOnLineItemUnitsInput;
+export type TCountOnLineItemUnitsDraftGraphql = Omit<
+  TCtpCountOnLineItemUnitsInput,
+  'excludeCount'
+>;
 
 export type TCreateCountOnLineItemUnitsDraftBuilder<
   TModel extends TCountOnLineItemUnitsDraftGraphql,
