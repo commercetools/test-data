e74aeed333bcdbd312e40261af69418de36fe3c9
Nicola Molinari <nicola.molinari@commercetools.com>
2025-04-30 21:04:44 +0200

    fix: more leftovers

--- models/product/src/product-data/search-keywords/index.ts
+++ models/product/src/product-data/search-keywords/index.ts
@@ -1,11 +1,14 @@
 import { RestModelBuilder, GraphqlModelBuilder } from './builders';
+import * as modelPresets from './presets';
 
 export * from './types';
 
 export const SearchKeywordsRest = {
   random: RestModelBuilder,
+  presets: modelPresets.restPresets,
 };
 
 export const SearchKeywordsGraphql = {
   random: GraphqlModelBuilder,
+  presets: modelPresets.graphqlPresets,
 };
