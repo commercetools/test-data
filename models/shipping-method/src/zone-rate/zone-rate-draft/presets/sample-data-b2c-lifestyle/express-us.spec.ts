31db9a058eaab20febbf39eae218d590ba80cd3b
Stephanie Sprinkle <stephanie.sprinkle@commercetools.com>
2025-05-08 09:30:39 -0400

    fix(shipping-method): zone rate tests

--- models/shipping-method/src/zone-rate/zone-rate-draft/presets/sample-data-b2c-lifestyle/express-us.spec.ts
+++ models/shipping-method/src/zone-rate/zone-rate-draft/presets/sample-data-b2c-lifestyle/express-us.spec.ts
@@ -4,12 +4,46 @@ import expressUsa from './express-us';
 describe('with express us preset', () => {
   it(`should return an express preset`, () => {
     const expressPreset = expressUsa().build<TZoneRateDraft>();
-    expect(expressPreset).toMatchInlineSnapshot();
+    expect(expressPreset).toMatchInlineSnapshot(`
+      {
+        "shippingRates": [
+          {
+            "freeAbove": undefined,
+            "price": {
+              "centAmount": 75000,
+              "currencyCode": "USD",
+            },
+            "tiers": [],
+          },
+        ],
+        "zone": {
+          "key": "usa",
+          "typeId": "zone",
+        },
+      }
+    `);
   });
 
   it(`should return an express preset when built for Graphql`, () => {
     const expressPresetGraphql =
       expressUsa().buildGraphql<TZoneRateDraftGraphql>();
-    expect(expressPresetGraphql).toMatchInlineSnapshot();
+    expect(expressPresetGraphql).toMatchInlineSnapshot(`
+      {
+        "shippingRates": [
+          {
+            "freeAbove": undefined,
+            "price": {
+              "centAmount": 75000,
+              "currencyCode": "USD",
+            },
+            "tiers": [],
+          },
+        ],
+        "zone": {
+          "key": "usa",
+          "typeId": "zone",
+        },
+      }
+    `);
   });
 });
