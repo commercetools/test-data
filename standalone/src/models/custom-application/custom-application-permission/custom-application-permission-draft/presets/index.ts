456e131b256324d2ea0c1da5ea0b27776c25d683
Levente Kobor <levente.kobor@commercetools.com>
2025-05-15 10:20:02 +0200

    chore: adjust preset naming

--- standalone/src/models/custom-application/custom-application-permission/custom-application-permission-draft/presets/index.ts
+++ standalone/src/models/custom-application/custom-application-permission/custom-application-permission-draft/presets/index.ts
@@ -2,6 +2,6 @@ import * as ManageOnlyPermissionsDraft from './manage-only-permissions-draft';
 import * as ViewOnlyPermissionsDraft from './view-only-permissions-draft';
 
 export const graphqlPresets = {
-  manageOnly: ManageOnlyPermissionsDraft.graphqlPreset,
-  viewOnly: ViewOnlyPermissionsDraft.graphqlPreset,
+  ManageOnlyPermissions: ManageOnlyPermissionsDraft.graphqlPreset,
+  ViewOnlyPermissions: ViewOnlyPermissionsDraft.graphqlPreset,
 };
