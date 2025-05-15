6ed5285a5e1251724431d281afe93aa39855e5be
Levente Kobor <levente.kobor@commercetools.com>
2025-05-15 11:21:20 +0200

    chore: use aliases

--- standalone/src/models/custom-application/custom-application-submenu-link/fields-config.ts
+++ standalone/src/models/custom-application/custom-application-submenu-link/fields-config.ts
@@ -1,19 +1,19 @@
-import { fake, Generator } from '@/core';
+import { fake, type TModelFieldsConfig } from '@/core';
 import { LocalizedField } from '@/models/commons';
 import { createRelatedDates } from '@/utils';
-import type { TCustomApplicationSubmenuLink } from './types';
+import type { TCustomApplicationSubmenuLinkGraphql } from './types';
 
 const [getOlderDate, getNewerDate] = createRelatedDates();
 
-export const graphqlFieldsConfig: TModelFieldsConfig<TMcSettingsCustomApplicationSubmenuLink> = {
-  fields: {
-    id: fake((f) => f.string.uuid()),
-    createdAt: fake(getOlderDate),
-    updatedAt: fake(getNewerDate),
-    uriPath: fake((f) => f.lorem.word()),
-    defaultLabel: fake((f) => f.lorem.word()),
-    labelAllLocales: fake(() => [LocalizedField.random()]),
-    permissions: [],
-  }
-}
-
+export const graphqlFieldsConfig: TModelFieldsConfig<TCustomApplicationSubmenuLinkGraphql> =
+  {
+    fields: {
+      id: fake((f) => f.string.uuid()),
+      createdAt: fake(getOlderDate),
+      updatedAt: fake(getNewerDate),
+      uriPath: fake((f) => f.lorem.word()),
+      defaultLabel: fake((f) => f.lorem.word()),
+      labelAllLocales: fake(() => [LocalizedField.random()]),
+      permissions: [],
+    },
+  };
