911eaae98cc1be67d38a6e0974cea68d4f43b5d2
rajrdk <rajrdk@gmail.com>
2025-05-16 17:39:03 +0530

    feat(): adding exports in index file

--- standalone/src/models/organization/organization/index.ts
+++ standalone/src/models/organization/organization/index.ts
@@ -1,3 +1,13 @@
-export * as McOrganization from './mc-organization';
 export * as Organization from './organization';
+export { default as presets } from './organization/presets';
+export * from './organization/types';
+
+// MC Organization
+export * as McOrganization from './mc-organization';
+export { default as presetsMc } from './mc-organization/presets';
+export * from './mc-organization/types';
+
+// Team
 export * as Team from './team';
+export { default as teamPresets } from './team/presets';
+export * from './team/types';
