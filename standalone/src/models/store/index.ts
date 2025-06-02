1228fd30e417c10176391d8ff9f39d694f8e4a64
Mario Arriola <mario.arriola@commercetools.com>
2025-06-02 14:46:21 +0200

    refactor(store): introduce new exported models for Store and ProductSelectionSetting with deprecation notices for legacy exports

--- standalone/src/models/store/index.ts
+++ standalone/src/models/store/index.ts
@@ -1,9 +1,65 @@
+import * as ProductSelectionSettingModel from './product-selection-setting';
+import * as ProductSelectionSettingDraftModel from './product-selection-setting/product-selection-setting-draft';
+import * as StoreModel from './store';
+import * as StoreDraftModel from './store/store-draft';
+
 // Export types
 export * from './store/types';
 export * from './product-selection-setting/types';
 
 // Export models
-export * as Store from './store';
-export * as StoreDraft from './store/store-draft';
-export * as ProductSelectionSetting from './product-selection-setting';
-export * as ProductSelectionSettingDraft from './product-selection-setting/product-selection-setting-draft';
+export const StoreRest = {
+  random: StoreModel.StoreRest.random,
+  presets: StoreModel.StoreRest.presets,
+};
+
+export const StoreGraphql = {
+  random: StoreModel.StoreGraphql.random,
+  presets: StoreModel.StoreGraphql.presets,
+};
+
+export const StoreDraftRest = {
+  random: StoreDraftModel.StoreDraftRest.random,
+  presets: StoreDraftModel.StoreDraftRest.presets,
+};
+
+export const StoreDraftGraphql = {
+  random: StoreDraftModel.StoreDraftGraphql.random,
+  presets: StoreDraftModel.StoreDraftGraphql.presets,
+};
+
+export const ProductSelectionSettingRest = {
+  random: ProductSelectionSettingModel.ProductSelectionSettingRest.random,
+  presets: ProductSelectionSettingModel.ProductSelectionSettingRest.presets,
+};
+
+export const ProductSelectionSettingGraphql = {
+  random: ProductSelectionSettingModel.ProductSelectionSettingGraphql.random,
+  presets: ProductSelectionSettingModel.ProductSelectionSettingGraphql.presets,
+};
+
+export const ProductSelectionSettingDraftRest = {
+  random: ProductSelectionSettingDraftModel.restPresets.random,
+  presets: ProductSelectionSettingDraftModel.restPresets.presets,
+};
+
+export const ProductSelectionSettingDraftGraphql = {
+  random: ProductSelectionSettingDraftModel.graphqlPresets.random,
+  presets: ProductSelectionSettingDraftModel.graphqlPresets.presets,
+};
+
+/**
+ * @deprecated Use `StoreRest` or `StoreGraphql` exported models instead of  `Store`
+ */
+export const Store = {
+  random: StoreModel.StoreRest.random,
+  presets: StoreModel.StoreRest.presets,
+};
+
+/**
+ * @deprecated Use `ProductSelectionSettingRest` or `ProductSelectionSettingGraphql` exported models instead of  `ProductSelectionSetting`
+ */
+export const ProductSelectionSetting = {
+  random: ProductSelectionSettingModel.ProductSelectionSettingRest.random,
+  presets: ProductSelectionSettingModel.ProductSelectionSettingRest.presets,
+};
