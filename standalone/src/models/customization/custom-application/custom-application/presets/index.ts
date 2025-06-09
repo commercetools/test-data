827141127d549ff864ffd09ba51ecb417497ac95
Carlos Cortizas <carlos.martines@commercetools.com>
2025-06-09 17:22:38 +0200

    refactor(custom-application): rename new preset

--- standalone/src/models/customization/custom-application/custom-application/presets/index.ts
+++ standalone/src/models/customization/custom-application/custom-application/presets/index.ts
@@ -1,9 +1,9 @@
 import * as withAllFieldsPresets from './with-all-fields/with-all-fields-preset';
 
 export const graphqlPresets = {
-  withAllFieldsPresets: withAllFieldsPresets.graphqlPreset,
+  withAllFields: withAllFieldsPresets.graphqlPreset,
 };
 
 export const compatPresets = {
-  withAllFieldsPresets: withAllFieldsPresets.compatPreset,
+  withAllFields: withAllFieldsPresets.compatPreset,
 };
