c679498e67bc281fb36674e108293878c2443927
Mario Arriola <mario.arriola@commercetools.com>
2025-06-11 06:27:58 +0200

    chore: update fields config on store model

--- standalone/src/models/store/store/fields-config.ts
+++ standalone/src/models/store/store/fields-config.ts
@@ -4,55 +4,49 @@ import {
   ClientLogging,
   LocalizedString,
   ReferenceGraphql,
-  TLocalizedStringGraphql,
 } from '@/models/commons';
 import { createRelatedDates } from '@/utils';
 import { TStoreRest, TStoreGraphql } from './types';
 
 const [getOlderDate, getNewerDate] = createRelatedDates();
 
+const commonFieldsConfig = {
+  id: fake((f) => f.string.uuid()),
+  version: sequence(),
+  key: fake((f) => f.lorem.slug()),
+  name: fake(() => LocalizedString.random()),
+  languages: [oneOf('en-US', 'de-DE', 'es-ES')],
+  countries: [fake((f) => ({ code: f.location.countryCode() }))],
+  distributionChannels: [fake(() => Channel.ChannelRest.random())],
+  supplyChannels: [fake(() => Channel.ChannelRest.random())],
+  createdAt: fake(getOlderDate),
+  lastModifiedAt: fake(getNewerDate),
+  productSelections: [],
+  custom: null,
+  createdBy: fake(() => ClientLogging.random()),
+  lastModifiedBy: fake(() => ClientLogging.random()),
+};
+
 export const restFieldsConfig: TModelFieldsConfig<TStoreRest> = {
   fields: {
-    id: fake((f) => f.string.uuid()),
-    version: sequence(),
-    key: fake((f) => f.lorem.slug()),
-    name: fake(() => LocalizedString.random()),
-    languages: [oneOf('en-US', 'de-DE', 'es-ES')],
-    countries: [fake((f) => ({ code: f.location.countryCode() }))],
-    distributionChannels: [fake(() => Channel.ChannelRest.random())],
-    supplyChannels: [fake(() => Channel.ChannelRest.random())],
-    createdAt: fake(getOlderDate),
-    lastModifiedAt: fake(getNewerDate),
-    lastModifiedBy: fake(() => ClientLogging.random()),
-    createdBy: fake(() => ClientLogging.random()),
-    productSelections: [],
-    custom: null,
+    ...commonFieldsConfig,
   },
 };
 
 export const graphqlFieldsConfig: TModelFieldsConfig<TStoreGraphql> = {
   fields: {
     __typename: 'Store',
-    id: fake((f) => f.string.uuid()),
-    version: sequence(),
-    key: fake((f) => f.lorem.slug()),
-    name: fake(() => LocalizedString.random()),
-    languages: [oneOf('en-US', 'de-DE', 'es-ES')],
-    countries: [fake((f) => ({ code: f.location.countryCode() }))],
-    productSelections: [],
-    custom: null,
-    createdAt: fake(getOlderDate),
+    ...commonFieldsConfig,
     lastModifiedAt: fake(getNewerDate),
-    createdBy: fake(() => ClientLogging.random()),
-    lastModifiedBy: fake(() => ClientLogging.random()),
     distributionChannels: [fake(() => Channel.ChannelRest.random())],
-    supplyChannels: [fake(() => Channel.ChannelRest.random())],
     distributionChannelsRef: null,
     supplyChannelsRef: null,
-    nameAllLocales: null,
+    nameAllLocales: fake((f) => LocalizedString.random()),
   },
   postBuild: (model) => {
-    const nameAllLocales = model.name as unknown as TLocalizedStringGraphql;
+    const name = model.nameAllLocales
+      ? LocalizedString.resolveGraphqlDefaultLocaleValue(model.nameAllLocales)
+      : model.nameAllLocales;
     const distributionChannelsRef = model.distributionChannels
       ? [
           ReferenceGraphql.presets
@@ -70,7 +64,7 @@ export const graphqlFieldsConfig: TModelFieldsConfig<TStoreGraphql> = {
         ]
       : undefined;
     return {
-      nameAllLocales,
+      name,
       distributionChannelsRef,
       supplyChannelsRef,
     };
