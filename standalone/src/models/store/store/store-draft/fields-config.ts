b774b69c7491603b29669810c0bb8d9e6cb8b879
Mario Arriola <mario.arriola@commercetools.com>
2025-06-11 06:33:08 +0200

    chore: extract in common field the common properties

--- standalone/src/models/store/store/store-draft/fields-config.ts
+++ standalone/src/models/store/store/store-draft/fields-config.ts
@@ -2,28 +2,27 @@ import { fake, oneOf, TModelFieldsConfig } from '@/core';
 import { LocalizedStringDraft } from '@/models/commons';
 import { TStoreDraftRest, TStoreDraftGraphql } from '../types';
 
+// https://docs.commercetools.com/api/projects/stores#storedraft
+
+const commonFieldsConfig = {
+  key: fake((f) => f.lorem.slug()),
+  name: fake(() => LocalizedStringDraft.random()),
+  languages: [oneOf('en-US', 'de-DE', 'es-ES', 'en-GB')],
+  countries: [fake((f) => f.location.countryCode())],
+  distributionChannels: null,
+  supplyChannels: null,
+  productSelections: null,
+  custom: null,
+};
+
 export const restFieldConfig: TModelFieldsConfig<TStoreDraftRest> = {
   fields: {
-    key: fake((f) => f.lorem.slug()),
-    name: fake(() => LocalizedStringDraft.random()),
-    languages: [oneOf('en-US', 'de-DE', 'es-ES', 'en-GB')],
-    countries: [fake((f) => f.location.countryCode())],
-    distributionChannels: null,
-    supplyChannels: null,
-    productSelections: null,
-    custom: null,
+    ...commonFieldsConfig,
   },
 };
 
 export const graphqlFieldConfig: TModelFieldsConfig<TStoreDraftGraphql> = {
   fields: {
-    key: fake((f) => f.lorem.slug()),
-    name: fake(() => LocalizedStringDraft.random()),
-    languages: [oneOf('en-US', 'de-DE', 'es-ES', 'en-GB')],
-    countries: [fake((f) => f.location.countryCode())],
-    distributionChannels: null,
-    supplyChannels: null,
-    productSelections: null,
-    custom: null,
+    ...commonFieldsConfig,
   },
 };
