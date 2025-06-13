07770ebf656134bf8f17ef2f8c7af691152b00bd
Nico <nicolas.lucero@commercetools.com>
2025-06-13 13:24:39 +0200

    fix(json-type): fix custom field value type add comment

--- standalone/src/models/custom-fields/raw-custom-field/types.ts
+++ standalone/src/models/custom-fields/raw-custom-field/types.ts
@@ -1,6 +1,7 @@
 import type { TBuilder } from '@/core';
 import { TCtpRawCustomField } from '@/graphql-types';
 
+// value is typed as JSON while it accepts any of the field types
 export type TRawCustomFielGraphql = Omit<TCtpRawCustomField, 'value'> & {
   value: unknown;
 };
