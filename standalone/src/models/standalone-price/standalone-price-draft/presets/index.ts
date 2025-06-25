afc6a2e9576112f8b1a1bcf8323e8435ee8de90d
Carlos Cortizas <carlos.martines@commercetools.com>
2025-06-25 12:34:16 +0200

    feat: remove standalone price model sample data presets

--- standalone/src/models/standalone-price/standalone-price-draft/presets/index.ts
+++ standalone/src/models/standalone-price/standalone-price-draft/presets/index.ts
@@ -1,7 +1,6 @@
 import empty from './empty';
 import full from './full';
-import sampleDataB2B from './sample-data-b2b';
 
-const presets = { empty, sampleDataB2B, full };
+const presets = { empty, full };
 
 export default presets;
