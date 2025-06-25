b107298cd197ae4d1a98f50cbced1afdfd700906
Carlos Cortizas <carlos.martines@commercetools.com>
2025-06-25 11:59:25 +0200

    feat: remove cart, order, payment and quotes sample data presets

--- standalone/src/models/cart/cart-discount/cart-discount/cart-discount-draft/presets/index.ts
+++ standalone/src/models/cart/cart-discount/cart-discount/cart-discount-draft/presets/index.ts
@@ -1,13 +1,7 @@
 import empty from './empty';
-import sampleDataB2B from './sample-data-b2b';
-import sampleDataB2CLifestyle from './sample-data-b2c-lifestyle';
-import sampleDataFashion from './sample-data-fashion';
 
 const presets = {
   empty,
-  sampleDataB2B,
-  sampleDataB2CLifestyle,
-  sampleDataFashion,
 };
 
 export default presets;
--- standalone/src/models/quote/staged-quote/staged-quote-draft/presets/index.ts
+++ standalone/src/models/quote/staged-quote/staged-quote-draft/presets/index.ts
@@ -1,6 +1,5 @@
 import empty from './empty';
-import sampleDataB2B from './sample-data-b2b';
 
-const presets = { empty, sampleDataB2B };
+const presets = { empty };
 
 export default presets;
