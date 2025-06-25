256202376f712b72b69e140933e7e51910dd1fc2
Carlos Cortizas <carlos.martines@commercetools.com>
2025-06-25 11:23:30 +0200

    feat: remove product sample data presets

--- standalone/src/models/product/product/product/product-draft/presets/index.ts
+++ standalone/src/models/product/product/product/product-draft/presets/index.ts
@@ -1,23 +1,11 @@
 import * as empty from './empty';
-import * as sampleDataB2B from './sample-data-b2b';
-import * as sampleDataB2CLifestyle from './sample-data-b2c-lifestyle';
-import * as sampleDataFashion from './sample-data-fashion';
 
 export const restPresets = {
   empty: empty.restPreset,
-  sampleDataB2B: sampleDataB2B.restPresets,
-  sampleDataB2CLifestyle: sampleDataB2CLifestyle.restPresets,
-  sampleDataFashion: sampleDataFashion.restPresets,
 };
 export const graphqlPresets = {
   empty: empty.graphqlPreset,
-  sampleDataB2B: sampleDataB2B.graphqlPresets,
-  sampleDataB2CLifestyle: sampleDataB2CLifestyle.graphqlPresets,
-  sampleDataFashion: sampleDataFashion.graphqlPresets,
 };
 export const compatPresets = {
   empty: empty.compatPreset,
-  sampleDataB2B: sampleDataB2B.compatPresets,
-  sampleDataB2CLifestyle: sampleDataB2CLifestyle.compatPresets,
-  sampleDataFashion: sampleDataFashion.compatPresets,
 };
