c4fd29030aea6fa08b874dce4256c6f93f1ceecb
Nicolas Lucero <nicolas.lucero-ext@commercetools.com>
2025-06-27 10:00:21 +0200

    [FEC-51]: migrating order models #7 - Delivery (#894)
    * refactor(delivery): initial changes
    
    * refactor(delivey): migrate model
    
    * test(typename): check for submodel typenames props

--- standalone/src/models/order/delivery/index.ts
+++ standalone/src/models/order/delivery/index.ts
@@ -1,5 +1,24 @@
-import LegacyBuilder from './builder';
+import {
+  RestModelBuilder,
+  GraphqlModelBuilder,
+  CompatDeliveryModelBuilder,
+} from './builders';
+import * as modelPresets from './presets';
 
+export const DeliveryRest = {
+  random: RestModelBuilder,
+  presets: modelPresets.restPresets,
+};
+
+export const DeliveryGraphql = {
+  random: GraphqlModelBuilder,
+  presets: modelPresets.graphqlPresets,
+};
+
+/**
+ * @deprecated use `DeliveryRest` or `DeliveryGraphql` instead
+ */
 export const Delivery = {
-  random: LegacyBuilder,
+  random: CompatDeliveryModelBuilder,
+  presets: modelPresets.compatPresets,
 };
