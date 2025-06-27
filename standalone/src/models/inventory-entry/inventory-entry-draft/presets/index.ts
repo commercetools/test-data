65c738c890b4a780ad034c30ca228e1bc5c08152
Carlos Cortizas <97907068+CarlosCortizasCT@users.noreply.github.com>
2025-06-27 10:43:51 +0200

    Remove sample data presets - batch 1 (#885)
    * feat: remove inventory-entry sample data presets
    
    * chore: add changeset

--- standalone/src/models/inventory-entry/inventory-entry-draft/presets/index.ts
+++ standalone/src/models/inventory-entry/inventory-entry-draft/presets/index.ts
@@ -1,25 +1,17 @@
 import * as changeHistoryData from './change-history-data';
 import * as empty from './empty';
-import * as sampleDataB2B from './sample-data-b2b';
-import * as sampleDataB2CLifestyle from './sample-data-b2c';
 
 export const restPresets = {
   changeHistoryData: changeHistoryData.restPresets,
   empty: empty.restPreset,
-  sampleDataB2B: sampleDataB2B.restPresets,
-  sampleDataB2CLifestyle: sampleDataB2CLifestyle.restPresets,
 };
 
 export const graphqlPresets = {
   changeHistoryData: changeHistoryData.graphqlPresets,
   empty: empty.graphqlPreset,
-  sampleDataB2B: sampleDataB2B.graphqlPresets,
-  sampleDataB2CLifestyle: sampleDataB2CLifestyle.graphqlPresets,
 };
 
 export const compatPresets = {
   changeHistoryData: changeHistoryData.compatPresets,
   empty: empty.compatPreset,
-  sampleDataB2B: sampleDataB2B.compatPresets,
-  sampleDataB2CLifestyle: sampleDataB2CLifestyle.compatPresets,
 };
