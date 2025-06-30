732c4a0a2805dcad8578cc2d7c971212abf81a7e
Carlos Cortizas <97907068+CarlosCortizasCT@users.noreply.github.com>
2025-06-30 17:06:06 +0200

    New VariantPricesListView test data model (#896)
    * feat: new variant-prices-list-view test data model
    
    * chore: add changeset

---
+++ standalone/src/models/my-view/variant-prices-list-view/index.ts
@@ -0,0 +1,7 @@
+import { GraphqlModelBuilder } from './builders';
+import * as presets from './presets';
+
+export const VariantPricesListViewGraphql = {
+  random: GraphqlModelBuilder,
+  presets: presets.graphqlPresets,
+};
