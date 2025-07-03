e98f1aa4e18f0ad92cf6fac41c2b9c3628c84688
Sarah Brolley <sarahbrolley@gmail.com>
2025-07-03 14:16:52 -0400

    feat(product-type): add tshirt preset

---
+++ standalone/src/models/product-type/product-type/presets/tshirt.ts
@@ -0,0 +1,41 @@
+import { type TBuilder } from '@/core';
+import { AttributeDefinitionRest, AttributeDefinitionGraphql } from '../..';
+import {
+  CompatModelBuilder,
+  GraphqlModelBuilder,
+  RestModelBuilder,
+} from '../builders';
+import type {
+  TProductType,
+  TProductTypeGraphql,
+  TProductTypeRest,
+} from '../types';
+
+export const restPreset = (): TBuilder<TProductTypeRest> =>
+  RestModelBuilder()
+    .name('T-shirt Product Type')
+    .attributes([
+      AttributeDefinitionRest.presets.countryOfOrigin().buildRest(),
+      AttributeDefinitionRest.presets.size().buildRest(),
+    ]);
+
+export const graphqlPreset = (): TBuilder<TProductTypeGraphql> =>
+  GraphqlModelBuilder()
+    .name('T-shirt Product Type')
+    .attributeDefinitions({
+      results: [
+        AttributeDefinitionGraphql.presets.countryOfOrigin().buildGraphql(),
+        AttributeDefinitionGraphql.presets.size().buildGraphql(),
+      ],
+      total: 1,
+      offset: 0,
+      __typename: 'AttributeDefinitionResult',
+    });
+
+export const compatPreset = (): TBuilder<TProductType> =>
+  CompatModelBuilder()
+    .name('T-shirt Product Type')
+    .attributes([
+      AttributeDefinitionRest.presets.countryOfOrigin().buildRest(),
+      AttributeDefinitionRest.presets.size().buildRest(),
+    ]);
