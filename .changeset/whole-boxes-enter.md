991b25364d33110bda748ed9f23a9c6e133220df
Sarah Brolley <sarahbrolley@gmail.com>
2025-07-08 12:23:29 -0400

    feat(cart): add index file to export cart and cart-discount models

---
+++ .changeset/whole-boxes-enter.md
@@ -0,0 +1,5 @@
+---
+'@commercetools/composable-commerce-test-data': minor
+---
+
+Add index file to cart to export cart and cart-discount models
---
+++ standalone/src/models/cart/index.ts
@@ -0,0 +1,5 @@
+export * from './cart-discount/cart-discount/types';
+export * from './cart/cart/types';
+
+export * from './cart-discount';
+export * from './cart';
