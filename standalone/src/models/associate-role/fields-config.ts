f713f66e7b6a03d4d77ac7ed6350927444b00526
Carlos Cortizas <carlos.martines@commercetools.com>
2025-07-14 10:37:56 +0200

    refactor(associate-role): remove unnecessary comments

--- standalone/src/models/associate-role/fields-config.ts
+++ standalone/src/models/associate-role/fields-config.ts
@@ -5,17 +5,6 @@ import type { TAssociateRoleGraphql, TAssociateRoleRest } from './types';
 
 const [getOlderDate, getNewerDate] = createRelatedDates();
 
-// buyerAssignable: Scalars['Boolean']['output'];
-// createdAt: Scalars['DateTime']['output'];
-// createdBy?: Maybe<TCtpInitiator>;
-// custom?: Maybe<TCtpCustomFieldsType>;
-// id: Scalars['String']['output'];
-// key: Scalars['String']['output'];
-// lastModifiedAt: Scalars['DateTime']['output'];
-// lastModifiedBy?: Maybe<TCtpInitiator>;
-// name?: Maybe<Scalars['String']['output']>;
-// permissions: Array<TCtpPermission>;
-// version: Scalars['Long']['output'];
 const commonFieldsConfig = {
   buyerAssignable: fake((f) => f.datatype.boolean()),
   createdAt: fake(getOlderDate),
@@ -30,35 +19,12 @@ const commonFieldsConfig = {
   version: sequence(),
 };
 
-// readonly id: string;
-// readonly version: number;
-// readonly createdAt: string;
-// readonly lastModifiedAt: string;
-// readonly lastModifiedBy?: LastModifiedBy;
-// readonly createdBy?: CreatedBy;
-// readonly key: string;
-// readonly buyerAssignable: boolean;
-// readonly name?: string;
-// readonly permissions: Permission[];
-// readonly custom?: CustomFields;
 export const restFieldsConfig: TModelFieldsConfig<TAssociateRoleRest> = {
   fields: {
     ...commonFieldsConfig,
   },
 };
 
-// __typename?: 'AssociateRole';
-// buyerAssignable: Scalars['Boolean']['output'];
-// createdAt: Scalars['DateTime']['output'];
-// createdBy?: Maybe<TCtpInitiator>;
-// custom?: Maybe<TCtpCustomFieldsType>;
-// id: Scalars['String']['output'];
-// key: Scalars['String']['output'];
-// lastModifiedAt: Scalars['DateTime']['output'];
-// lastModifiedBy?: Maybe<TCtpInitiator>;
-// name?: Maybe<Scalars['String']['output']>;
-// permissions: Array<TCtpPermission>;
-// version: Scalars['Long']['output'];
 export const graphqlFieldsConfig: TModelFieldsConfig<TAssociateRoleGraphql> = {
   fields: {
     ...commonFieldsConfig,
