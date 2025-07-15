34d77b71345c8e7282f2d202d3d4b396ba1b5035
Nico <nicolas.lucero@commercetools.com>
2025-07-15 15:12:56 +0200

    fix(shopping-lists): exporting missing submodels

--- standalone/src/models/shopping-list/index.ts
+++ standalone/src/models/shopping-list/index.ts
@@ -5,5 +5,8 @@ export * from './text-line-item/types';
 
 // Export models
 export * as ShoppingList from './shopping-list';
+export * as ShoppingListDraft from './shopping-list/shopping-list-draft';
 export * as ShoppingListLineItem from './shopping-list-line-item';
+export * as ShoppingListLineItemDraft from './shopping-list-line-item/shopping-list-line-item-draft';
 export * as TextLineItem from './text-line-item';
+export * as TextLineItemDraft from './text-line-item/text-line-item-draft';
