89b18bcbb083b8181f2b91d00cac4a333c420f7a
Nico <nicolas.lucero@commercetools.com>
2025-07-15 16:05:39 +0200

    fix(shopping-lists): export submodels

--- standalone/src/models/shopping-list/index.ts
+++ standalone/src/models/shopping-list/index.ts
@@ -4,9 +4,9 @@ export * from './shopping-list-line-item/types';
 export * from './text-line-item/types';
 
 // Export models
-export * as ShoppingList from './shopping-list';
-export * as ShoppingListDraft from './shopping-list/shopping-list-draft';
-export * as ShoppingListLineItem from './shopping-list-line-item';
-export * as ShoppingListLineItemDraft from './shopping-list-line-item/shopping-list-line-item-draft';
-export * as TextLineItem from './text-line-item';
-export * as TextLineItemDraft from './text-line-item/text-line-item-draft';
+export * from './shopping-list';
+export * from './shopping-list/shopping-list-draft';
+export * from './shopping-list-line-item';
+export * from './shopping-list-line-item/shopping-list-line-item-draft';
+export * from './text-line-item';
+export * from './text-line-item/text-line-item-draft';
