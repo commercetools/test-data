006156f20c79345520669a13157396909531243d
Carlos Cortizas <carlos.martines@commercetools.com>
2025-07-23 18:35:41 +0200

    refactor(order): adjust payment-info fields configuration

--- standalone/src/models/order/payment-info/fields-config.ts
+++ standalone/src/models/order/payment-info/fields-config.ts
@@ -1,4 +1,5 @@
 import { fake, type TModelFieldsConfig } from '@/core';
+import { ReferenceGraphql, ReferenceRest } from '@/models/commons';
 import { Payment } from '@/models/payment';
 import type { TPaymentInfoGraphql, TPaymentInfoRest } from './types';
 
@@ -9,13 +10,37 @@ const commonFieldsConfig = {
 export const restFieldsConfig: TModelFieldsConfig<TPaymentInfoRest> = {
   fields: {
     ...commonFieldsConfig,
+    payments: fake(() => {
+      return [ReferenceRest.presets.paymentReference().obj(Payment.random())];
+    }),
+  },
+  postBuild: (model) => {
+    // Make sure the built payments IDs are set in the references objects
+    const payments = model.payments.map((payment) => ({
+      ...payment,
+      id: payment.obj?.id,
+    }));
+    return {
+      ...model,
+      payments: payments as TPaymentInfoRest['payments'],
+    };
   },
 };
 
 export const graphqlFieldsConfig: TModelFieldsConfig<TPaymentInfoGraphql> = {
   fields: {
-    __typename: 'PaymentInfo',
     ...commonFieldsConfig,
+    payments: fake(() => [Payment.random()]),
     paymentRefs: null,
+    __typename: 'PaymentInfo',
+  },
+  postBuild: (model) => {
+    const paymentRefs = model.payments.map((payment) =>
+      ReferenceGraphql.presets.paymentReference().id(payment.id).build()
+    );
+    return {
+      ...model,
+      paymentRefs,
+    };
   },
 };
