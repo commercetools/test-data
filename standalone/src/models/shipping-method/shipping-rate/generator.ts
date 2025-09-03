024e68370250c936014a0d4b2d32de7c0bd3c4aa
Lucas Grahn <lucas.grahn@commercetools.com>
2025-09-03 13:58:44 +0200

    fix: make use of new models inside shipping drafts

--- standalone/src/models/shipping-method/shipping-rate/generator.ts
+++ standalone/src/models/shipping-method/shipping-rate/generator.ts
@@ -1,5 +1,6 @@
 import { Generator, fake } from '@/core';
-import { CentPrecisionMoney, MoneyDraftRest } from '@/models/commons';
+import { CentPrecisionMoney } from '@/models/commons';
+import { CartScorePriceTierDraftRest } from './shipping-rate-price-tier/cart-score';
 import { TShippingRate } from './types';
 
 // https://docs.commercetools.com/api/projects/shippingMethods#shippingrate
@@ -8,13 +9,7 @@ const generator = Generator<TShippingRate>({
   fields: {
     price: fake(() => CentPrecisionMoney.random()),
     freeAbove: fake(() => CentPrecisionMoney.random()),
-    tiers: fake(() => [
-      {
-        type: 'CartScore',
-        price: fake(() => MoneyDraftRest.presets.withCurrency('EUR')),
-        score: fake((f) => f.number.int({ min: 1, max: 10 })),
-      },
-    ]),
+    tiers: fake(() => [CartScorePriceTierDraftRest.random().build()]),
     isMatching: fake((f) => f.datatype.boolean()),
   },
 });
