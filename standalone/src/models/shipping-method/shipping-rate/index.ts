39d047c170bb9d3944a1eeebd944f7fe7d661f32
Lucas Grahn <lucas.grahn@commercetools.com>
2025-09-04 15:01:44 +0200

    test: migrate shippingratedraft and shipping rate to new model

--- standalone/src/models/shipping-method/shipping-rate/index.ts
+++ standalone/src/models/shipping-method/shipping-rate/index.ts
@@ -1,6 +1,14 @@
+import { RestModelBuilder, GraphqlModelBuilder } from './builders';
+export * from './types';
+
 export * as ShippingRateDraft from './shipping-rate-draft';
 export * as ShippingRatePriceTierDraft from './shipping-rate-price-tier';
 
-export { default as random } from './builder';
-export { default as presets } from './presets';
-export * from './types';
+// Main Product Tailoring models
+export const ShippingRateRest = {
+  random: RestModelBuilder,
+};
+
+export const ShippingRateGraphql = {
+  random: GraphqlModelBuilder,
+};
