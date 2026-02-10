4c52c0cb09286c7d504b404b002ef645ee71bb85
Nima Jahanshahlou <nima.jahanshahlou@commercetools.com>
2026-02-10 09:16:10 +0100

    fix(jest): update transformIgnorePatterns for ESM compatibility with @faker-js/faker

--- jest.test.config.js
+++ jest.test.config.js
@@ -6,6 +6,11 @@ module.exports = {
   transform: {
     '^.+\\.(js|ts)$': 'babel-jest',
   },
+  // @faker became ESM only starting from v10,
+  // so we need to allow Jest to transform it.
+  transformIgnorePatterns: [
+    '/node_modules/(?!@faker-js/faker)(?!.pnpm/.*/@faker-js/faker)',
+  ],
   testMatch: ['**/*.spec.js', '**/*.spec.ts'],
   moduleFileExtensions: ['ts', 'js'],
   watchPlugins: ['jest-watch-typeahead/filename'],
